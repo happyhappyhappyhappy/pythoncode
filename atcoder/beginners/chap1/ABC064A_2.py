b1fa3377c24a036dc6edeff1bf76cd40bb9f5449
ymnkkj <ymnkkj@gmail.com>
2019-12-06 14:28:40 +0900

    ABC064A 2回目終了 201912061428

--- atcoder/beginners/chap1/ABC064A_2.py
+++ atcoder/beginners/chap1/ABC064A_2.py
@@ -1,2 +1,14 @@
 # Problem https://atcoder.jp/contests/abc064/tasks/abc064_a
 # Python 2nd Try
+yes = "YES"
+no = "NO"
+if __name__ == "__main__":
+    r, g, b = map(int, input().split(' '))
+    answer = ""
+    checkNo = (g*10+b) % 4
+    if checkNo == 0:
+        answer = yes
+    else:
+        answer = no
+    print(answer)
+exit
--- atcoder/beginners/chap1/ABC064A_MEMO.md
+++ atcoder/beginners/chap1/ABC064A_MEMO.md
@@ -11,3 +11,11 @@
 * [完了](https://atcoder.jp/contests/abc064/submissions/8697212)
 * 実行時間17ms,メモリ3060kb
     * これが標準か
+---
+## 2回目
+# 2019/12/06
+* 今回は素直に3つの変数を用いる
+* 後、mainの導入
+* [完了](https://atcoder.jp/contests/abc064/submissions/8806801)
+    * 実行時間は変わらないが、メモリが2940kbと少なくなった。
+        * 以前3060kbになっていたのはlistのオブジェクトの影響か
