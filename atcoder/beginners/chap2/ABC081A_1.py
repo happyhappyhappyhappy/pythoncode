5161e408a55ff2910292a231911526728c655bac
ymnkkj <ymnkkj@gmail.com>
2019-12-08 18:48:03 +0900

    listができあがらない 201912081847

--- atcoder/beginners/chap2/ABC081A_1.py
+++ atcoder/beginners/chap2/ABC081A_1.py
@@ -1,2 +1,21 @@
-# Problem
-#
+# Problem https://atcoder.jp/contests/abc081/tasks/abc081_a
+# Python 1st Try
+
+
+def onecounter(input_square):
+    strList = list(map(int, input_square.split('')))
+    print(strList)
+    result = 0
+    if strList[0] == 1:
+        result = result + 1
+    if strList[1] == 1:
+        result = result + 1
+    if strList[2] == 1:
+        result = result + 1
+    return result
+
+
+if __name__ == "__main__":
+    answer = onecounter(input())
+    print(answer)
+exit
--- atcoder/beginners/chap2/ABC081A_MEMO.md
+++ atcoder/beginners/chap2/ABC081A_MEMO.md
@@ -1,8 +1,10 @@
-# ABC082A-Round Up the Mean
+# ABC081A-Placing Marbles
 
-[Problem](https://atcoder.jp/contests/abc082/tasks/abc082_a)
+[Problem](https://atcoder.jp/contests/abc081/tasks/abc081_a)
 
 ---
 ## 1回目
 
 # 2019/12/08
+* listを使って見る
+* [完了]()
