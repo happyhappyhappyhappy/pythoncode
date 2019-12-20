a6e40d8e36d431340b866f48f798868561ded589
ymnkkj <ymnkkj@gmail.com>
2019-12-20 18:52:01 +0900

    ABC085A 1回目ひな形 201912201851

---
+++ atcoder/beginners/chap2/ABC085A_1.py
@@ -0,0 +1,13 @@
+# Problem https://atcoder.jp/contests/abc085/tasks/abc085_a
+# Python 1st Try
+
+
+def solver(errorDay):
+    result = "2018/01/"
+    return result
+
+
+if __name__ == "__main__":
+    result = ""
+    result = solver(input())
+    print(result)
---
+++ atcoder/beginners/chap2/ABC085A_MEMO.md
@@ -0,0 +1,8 @@
+# ABC085A-Already 2018
+
+[Problem](https://atcoder.jp/contests/abc085/tasks/abc085_a)
+
+---
+## 1回目
+# 2019/12/20
+
