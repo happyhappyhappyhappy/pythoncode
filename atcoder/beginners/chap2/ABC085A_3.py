244c5ce12281d165923c60cc6f72fbada48e879b
ymnkkj <ymnkkj@gmail.com>
2019-12-26 18:53:41 +0900

    ABC085A 3回目完了 201912261853

--- atcoder/beginners/chap2/ABC085A_3.py
+++ atcoder/beginners/chap2/ABC085A_3.py
@@ -1,16 +1,17 @@
 # Problem https://atcoder.jp/contests/abc085/tasks/abc085_a
 # Python 3rd Try
+import copy
 
 
 def solver(errorStr):
-    result = ""
     strList = list(errorStr)
-    for i in range(0, *):
-        pass
+    trueList = copy.deepcopy(strList)
+    trueList[3] = '8'
+    result = ''.join(trueList)
     return result
 
 
 if __name__ == "__main__":
     result = ""
     result = solver(input())
-    print(solver)
+    print(result)
