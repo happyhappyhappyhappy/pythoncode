63041eda36b293add2e233eb233127cd1631f82a
Yamanaka Kohji <ymnkkj@gmail.com>
2020-01-04 19:56:49 +0900

    202001041956 ABC069B 3回目途中

---
+++ atcoder/beginners/chap2/ABC069B_3.py
@@ -0,0 +1,15 @@
+# Problem 
+# Python 3rd Try
+
+
+def solver(inputStr) :
+    top = ""
+    middle = ""
+    endChar = ""
+    return top,middle,endChar
+
+
+if __init__=="__main__" :
+
+
+
--- atcoder/beginners/chap2/pastfiles/ABC069B_2.py
+++ atcoder/beginners/chap2/pastfiles/ABC069B_2.py
@@ -10,3 +10,15 @@ def solver(inputStr):
 if __name__ == "__main__":
     result = solver(input())
     print(result)
+# Problem https://atcoder.jp/contests/abc069/tasks/abc069_b
+# Python 2nd Try
+
+
+def solver(inputStr):
+    result = inputStr[0] + str(len(inputStr)-2) + inputStr[-1]
+    return result
+
+
+if __name__ == "__main__":
+    result = solver(input())
+    print(result)
