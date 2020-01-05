b4fc9d1e3af9bfb640c2ca44f3798c2a4368cc22
Yamanaka Kohji <ymnkkj@gmail.com>
2020-01-05 19:56:23 +0900

    ABC069B 3回目完了 202001051956

--- atcoder/beginners/chap2/ABC069B_3.py
+++ atcoder/beginners/chap2/ABC069B_3.py
@@ -1,15 +1,21 @@
-# Problem 
+# Problem  https://atcoder.jp/contests/abc069/tasks/abc069_b
 # Python 3rd Try
 
 
 def solver(inputStr) :
-    top = ""
-    middle = ""
-    endChar = ""
+    top =  inputStr[0]
+    middle = len(inputStr)-2
+    endChar = inputStr[-1]
     return top,middle,endChar
 
 
-if __init__=="__main__" :
+if __name__=="__main__" :
+    answer1=''
+    answer2=0
+    answer3=''
+    S = input()
+    answer1,answer2,answer3= solver(S)
+    print(answer1,answer2,answer3,sep='')
 
 
 
