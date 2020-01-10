9754f7e6e555c050ddc02a77a5b6110bcb891e7c
Yamanaka Kohji <ymnkkj@gmail.com>
2020-01-10 18:57:05 +0900

    ABC081B 1回目開始直後 202001101857

--- atcoder/beginners/Chap3/ABC081B_1.py
+++ atcoder/beginners/Chap3/ABC081B_1.py
@@ -1,2 +1,16 @@
-# Problem
+# Problem https://atcoder.jp/contests/abc081/tasks/abc081_b
 # Python 1st Try
+import sys
+
+# def solve(allNumber,eachNum):
+#    answer = ""
+#    return answer
+
+
+if __name__ == "__main__":
+    input=sys.stdin.readline
+    N = input().split()
+    Ai = input().split()
+    print(N)
+    print(Ai)
+    print("End")
---
+++ atcoder/beginners/Chap3/ABC081B_MEMO.md
@@ -0,0 +1,9 @@
+# ABC081B-Shift only
+
+[Problem](https://atcoder.jp/contests/abc081/tasks/abc081_b)
+
+---
+## 1回目
+
+# 2020/01/10
+* おすすめであるという`readline`入力でトライ
