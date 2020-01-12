10267387a3a42cbdf4df9df8d4a7bd2b782b8ff0
Yamanaka Kohji <ymnkkj@gmail.com>
2020-01-12 18:50:42 +0900

    引き続き1問目途中

--- atcoder/beginners/Chap3/ABC081B_1.py
+++ atcoder/beginners/Chap3/ABC081B_1.py
@@ -2,15 +2,23 @@
 # Python 1st Try
 import sys
 
-# def solve(allNumber,eachNum):
-#    answer = ""
-#    return answer
+def solve(allNumber,eachNum):
+    answer = 0
+    counter = [0]*allNumber
+    for j in range(len(eachNum)):
+        cnt = 0
+        while eachNum[j] % 2 == 0:
+            counter[j] = counter[j] + 1
+            eachNum[j] = eachNum[j] / 2
+#    print(counter)
+#    answer [Todo] ここに最大値を設定してcounter配列を回す
+    return answer
 
 
 if __name__ == "__main__":
     input=sys.stdin.readline
     N = list(map(int,input().split()))
     Ai = list(map(int,input().split()))
-    print(N[0])
-    print(Ai)
-    print("End")
+#    print(N[0])
+#    print(Ai)
+    print(solve(N[0],Ai))
