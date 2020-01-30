3b0dbd34bb66719a6c7ba1e30d45ef9c3c8565b9
Yamanaka Kohji <ymnkkj@gmail.com>
2020-01-30 19:25:56 +0900

    ABC068B 3回目謎の挙動 202001301925

--- atcoder/beginners/Chap3/ABC068B_3.py
+++ atcoder/beginners/Chap3/ABC068B_3.py
@@ -1,2 +1,28 @@
-# Problem
+# Problem https://atcoder.jp/contests/abc068/tasks/abc068_b
 # python 3rd Try
+
+
+import sys
+
+
+def twoDividedCounter(N):
+    answer = 0
+    copyNumber = N
+    while True:
+        print(N+"check")
+        if copyNumber % 2 == 0:
+            answer = answer + 1
+            copyNumber = copyNumber / 2
+        else:
+            break
+    return answer
+
+
+def solver(N):
+    answer = twoDividedCounter(N)
+    return answer
+
+
+if __name__ == "__main__":
+    N = list(map(int, sys.stdin))
+    print(solver(N[0]))
