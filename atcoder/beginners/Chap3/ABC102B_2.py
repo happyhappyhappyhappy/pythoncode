6b287657a43c889fa47b5e298cbddfba95ab005e
Yamanaka Kohji <ymnkkj@gmail.com>
2020-02-01 19:49:44 +0900

    202002011941段階 ABC102B 2回目

--- atcoder/beginners/Chap3/ABC102B_2.py
+++ atcoder/beginners/Chap3/ABC102B_2.py
@@ -1,3 +1,17 @@
 # Problem https://atcoder.jp/contests/abc102/tasks/abc102_b
 # Python 2nd Try
 import sys
+
+
+def solver(allN, numberList):
+    answer = 0
+# for forを二重回転させる
+#
+#
+    return answer
+
+
+if __name__ == "__main__":
+    N = sys.stdin.readline().rsplit()
+    Ai = list(map(int, sys.stdin.readline().split()))
+    print(solver(int(N[0])), Ai)
