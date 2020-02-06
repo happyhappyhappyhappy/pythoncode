864ad061012d51f09fbf20034e962a8d453e230c
Yamanaka Kohji <ymnkkj@gmail.com>
2020-02-06 19:49:51 +0900

    ABC113B 1回目完了 202002061949

--- atcoder/beginners/Chap3/ABC113B_1.py
+++ atcoder/beginners/Chap3/ABC113B_1.py
@@ -3,18 +3,20 @@
 import sys
 
 
-def solver(allNum, MaxTemp, TargetTemp, heightPlace):
-    answer = 0
-    minDiff = MaxTemp
-    for j in range(0, allNum):
-        diffTemp = abs(TargetTemp - (MaxTemp-heightPlace[j]*0.006))
+def solver(allnum, maxtemp, targettemp, heightplace):
 
+    answer = 0
+    mindiff = 100000000
+    for j in range(0, allnum):
+        difftemp = abs(targettemp - (maxtemp-heightplace[j]*0.006))
+        if difftemp < mindiff:
+            mindiff = difftemp
+            answer = j
     return answer+1
 
 
 if __name__ == "__main__":
     N = list(map(int, sys.stdin.readline().rsplit()))
-    print(type(N))
-    sLine = list(map(float, sys.stdin.readline().split()))
-    Hi = list(map(float, sys.stdin.readline().split()))
-    print(solver(N[0], sLine[0], sLine[1], Hi))
+    SLINE = list(map(float, sys.stdin.readline().split()))
+    HI = list(map(float, sys.stdin.readline().split()))
+    print(solver(N[0], SLINE[0], SLINE[1], HI))
