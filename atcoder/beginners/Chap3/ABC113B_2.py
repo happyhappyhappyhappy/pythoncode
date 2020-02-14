013d0c94bd4a557febea66eb8a75aea86bbdfb83
Yamanaka Kohji <ymnkkj@gmail.com>
2020-02-14 19:27:59 +0900

    ABC113B 3回目終了 202002141927

--- atcoder/beginners/Chap3/ABC113B_2.py
+++ atcoder/beginners/Chap3/ABC113B_2.py
@@ -5,12 +5,12 @@ import sys
 
 def solver(allplace, maxtemp, targettemp, eachheight):
     answer = 0
-    difftemp = maxtemp # これNG。対策はしょうが無いが素直にfloat変数としての最大値を使う必要あり
+    # difftemp = maxtemp # これNG。対策はしょうが無いが素直にfloat変数としての最大値を使う必要あり
     # sample2にて最大21,0の設定が何時までも続いてしまうため
+    difftemp = 10.0**5
     for j in range(0, allplace):
         heretemp = abs(targettemp - (maxtemp - eachheight[j]*0.006))
-        print(difftemp, heretemp)
-        if heretemp < difftemp:
+        if heretemp <= difftemp:
             answer = j
             difftemp = heretemp
     return answer+1
