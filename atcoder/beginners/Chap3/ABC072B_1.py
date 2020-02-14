d20ae9e4ac9f26a4804ce0151e03b5703d1d8dd7
Yamanaka Kohji <ymnkkj@gmail.com>
2020-02-14 19:59:31 +0900

    ABC072B 1回目着手 202002141959

---
+++ atcoder/beginners/Chap3/ABC072B_1.py
@@ -0,0 +1,15 @@
+# Problem https://atcoder.jp/contests/abc072/tasks/abc072_b
+# Python 1st Try
+import sys
+
+
+def solver(inputStr):
+    answer = ''
+    for j in range(0,len(inputStr),2:
+        answer
+    return answer
+
+
+if __name__ == "__main__":
+    s = sys.stdin.readline().split()
+    print(solver(s[0]))
---
+++ atcoder/beginners/Chap3/ABC072B_MEMO.md
@@ -0,0 +1,8 @@
+# ABC072B-OddString
+
+[Problem](https://atcoder.jp/contests/abc072/tasks/abc072_b)
+
+---
+## 1回目
+# 2020/02/14
+* とりあえず形整えただけで終わり
