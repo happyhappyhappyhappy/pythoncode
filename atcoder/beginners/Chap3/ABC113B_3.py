36534b9ad7608cbfe3542859686582884c1508bf
Yamanaka Kohji <ymnkkj@gmail.com>
2020-02-18 20:02:14 +0900

    20200218完了段階

---
+++ atcoder/beginners/Chap3/ABC113B_3.py
@@ -0,0 +1,15 @@
+# Problem https://atcoder.jp/contests/abc113/tasks/abc113_b
+# Python 3rd Try
+
+class Problem:
+    def __init__(self, N, T, A, Hi):
+
+        pass
+    pass
+
+if __name__ == "__main__":
+    N = 0
+    T = 0
+    A = 0
+    Hi = []
+    print(Problem(N,T,A,Hi).solve())
