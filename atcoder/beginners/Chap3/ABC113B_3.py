952d884710b0b5202389dd9b6b850ef041a1ee2a
Yamanaka Kohji <ymnkkj@gmail.com>
2020-02-19 19:53:28 +0900

    ABC113B 3回目 クラス作成と内容確認 202002191953

--- atcoder/beginners/Chap3/ABC113B_3.py
+++ atcoder/beginners/Chap3/ABC113B_3.py
@@ -1,15 +1,25 @@
 # Problem https://atcoder.jp/contests/abc113/tasks/abc113_b
 # Python 3rd Try
 
-class Problem:
-    def __init__(self, N, T, A, Hi):
 
-        pass
-    pass
+class Problem:
+    def __init__(self, allnumber, maxthermo, avepoint, highs):
+        self.allnumber = allnumber
+        self.maxthermo = maxthermo
+        self.avepoint = avepoint
+        self.highs = highs
+
+    def problemprint(self):
+        print(self.allnumber)
+        print(self.maxthermo)
+        print(self.avepoint)
+        print(self.highs)
+
 
 if __name__ == "__main__":
-    N = 0
-    T = 0
-    A = 0
-    Hi = []
-    print(Problem(N,T,A,Hi).solve())
+    N = 10
+    T = 20
+    A = 30
+    HI = [1, 2, 3, 4]
+#    print(Problem(N, T, A, HI).solve())
+    Problem(N, T, A, HI).problemprint()
