819c72669a8800f3df72fa141f12cb2b4d6cab20
Yamanaka Kohji <ymnkkj@gmail.com>
2020-03-06 19:51:06 +0900

    ABC095B 1回目 環境作成 202003061951

---
+++ atcoder/beginners/Chap3/ABC095B_1.py
@@ -0,0 +1,11 @@
+# Problem https://atcoder.jp/contests/abc095/tasks/abc095_b
+# Python 1st Try
+
+
+def solver(kinddonus, eachsource, havesource):
+    result = 0
+    return result
+
+
+if __name__ == "__main__":
+    N = list(map(int, input().split(' ')))
---
+++ atcoder/beginners/Chap3/ABC095B_MEMO.md
@@ -0,0 +1,3 @@
+# ABC095B-Bitter Alchemy
+[Problem](https://atcoder.jp/contests/abc095/tasks/abc095_b)
+-----
