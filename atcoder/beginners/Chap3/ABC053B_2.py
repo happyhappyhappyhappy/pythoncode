91d33c69b21a47d998e588985a8be7f06f8e177f
Yamanaka Kohji <ymnkkj@gmail.com>
2020-03-06 19:36:57 +0900

    ABC053B 2回目終了 20200306193650

--- atcoder/beginners/Chap3/ABC053B_2.py
+++ atcoder/beginners/Chap3/ABC053B_2.py
@@ -1,2 +1,23 @@
 # Problem https://atcoder.jp/contests/abc053/tasks/abc053_b
 # Python 2nd Try
+
+
+def solver(string):
+    result = 200000
+    aposi = 0
+    zposi = len(string)
+    for j in range(0, len(string), 1):
+        if string[j] == 'A':
+            aposi = j
+            break
+    for j in range(len(string)-1, -1, -1):
+        if string[j] == 'Z':
+            zposi = j
+            break
+    result = zposi - aposi + 1
+    return result
+
+
+if __name__ == "__main__":
+    s = input()
+    print('{}'.format(solver(s)))
