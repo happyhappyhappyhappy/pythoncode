d065fa5d2c4864c4f5ed1ad3edb02dc80cfb8ba9
Yamanaka Kohji <ymnkkj@gmail.com>
2020-03-07 19:44:05 +0900

    ABC095B 1回目終了 202003071943

--- atcoder/beginners/Chap3/ABC095B_1.py
+++ atcoder/beginners/Chap3/ABC095B_1.py
@@ -4,8 +4,19 @@
 
 def solver(kinddonus, eachsource, havesource):
     result = 0
+    allsource = 0
+    minsource = 1000
+    for j in range(0, kinddonus, +1):
+        allsource = eachsource[j] + allsource
+        if eachsource[j] <= minsource:
+            minsource = eachsource[j]
+    result = kinddonus + (havesource - allsource) // minsource
     return result
 
 
 if __name__ == "__main__":
-    N = list(map(int, input().split(' ')))
+    N, X = list(map(int, input().split(' ')))
+    M = []
+    for j in range(0, N, +1):
+        M.append(int(input()))
+    print(solver(N, M, X))
--- atcoder/beginners/Chap3/ABC095B_MEMO.md
+++ atcoder/beginners/Chap3/ABC095B_MEMO.md
@@ -1,3 +1,10 @@
 # ABC095B-Bitter Alchemy
 [Problem](https://atcoder.jp/contests/abc095/tasks/abc095_b)
 -----
+## 1回目
+# 2020/03/07
+* まずは入力データは一行目は`input()`使って、二つの変数にする
+* それ以降は`readlines`の手法が使えそうだが、まず一回目は様子見でNの数だけ回して、次々listにappendする[参考](https://qiita.com/rsakamot/items/2277e26e3716e8f8f5a2#append)
+* [完了](https://atcoder.jp/contests/abc095/submissions/10585886)
+    * 実行時間は全部17ms,メモリは一部3064kb
+* 割り算の演算子は`/ と // ` 前者は小数まで出す。後者は切り捨て整数。[ソース元確認](http://www.tohoho-web.com/python/operators.html)、本家は気が向いたら探す。
