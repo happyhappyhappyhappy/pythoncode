19d055de78b0e7f534cb5417d9ea25fe07d3bf4d
Yamanaka Kohji <ymnkkj@gmail.com>
2020-03-27 18:43:51 +0900

    20200327

--- atcoder/beginners/chap4/ARC004A_1.py
+++ atcoder/beginners/chap4/ARC004A_1.py
@@ -2,6 +2,8 @@
 # Python 1st Try
 
 import sys
+import math
+import pprint
 # from collections import defaultdict
 # import heapq,copy
 # from collections import deque
@@ -9,16 +11,33 @@ int1 = lambda x: int(x) - 1
 p2D = lambda x: print(*x, sep="\n")
 def II(): return int(sys.stdin.readline())
 def MI(): return map(int, sys.stdin.readline().split())
-def MI1(): return map(int1, sys.stdin.readline().split())
+# def MI1(): return map(int1, sys.stdin.readline().split())
 def LI(): return list(map(int, sys.stdin.readline().split()))
 def LLI(rows_number): return [LI() for _ in range(rows_number)]
 
 
-def solver():
-    result = 0
+def solver(point, xs, ys):
+    result = 0.0000000
     # algorithm
+    for j in range(0,point):
+        for k in range(0,point):
+            nowx = xs[j] - xs[k]
+            nowy = ys[j] - ys[k]
+            nowselect = math.sqrt(nowx*nowx+nowy*nowy)
+#             print("{:6}".format(nowselect))
+            if result < nowselect:
+                result = nowselect
     return result
 
 
 if __name__ == "__main__":
-    print("{}".format(solver()))
+    N = II()
+    X = []
+    Y = []
+    for j in range(0,N):
+        nowx , nowy =  MI()
+        X.append(nowx)
+        Y.append(nowy)
+    print("{:6f}".format(solver(N, X, Y)))
+#    pprint.pprint(X)
+#    pprint.pprint(Y)
--- atcoder/beginners/chap4/ARC004A_MEMO.md
+++ atcoder/beginners/chap4/ARC004A_MEMO.md
@@ -2,4 +2,9 @@
 [Problem](https://atcoder.jp/contests/arc004/tasks/arc004_1)
 -----
 ## 1回目
-#
+# 2020/3/27
+* 最初出力がおかしかったが原因を見れば二点の差を出すのが目的だった
+    * 最初は(0,0)から全点を見ていた
+    * 落ち着けば分かった
+* [完了](https://atcoder.jp/contests/arc004/submissions/11229171)
+    * python3は29ms,3572kbで完了のまき
