95ff6711bd107d83864e2c241fd07d2f472ca9d3
Yamanaka Kohji <ymnkkj@gmail.com>
2020-04-10 19:41:03 +0900

    ABC080B 1回目完了 202004101940

--- atcoder/beginners/chap5/ABC080B_1.py
+++ atcoder/beginners/chap5/ABC080B_1.py
@@ -11,12 +11,20 @@ def II(): return int(sys.stdin.readline())
 def MI(): return map(int, sys.stdin.readline().split())
 def MI1(): return map(int1, sys.stdin.readline().split())
 def LI(): return list(map(int, sys.stdin.readline().split()))
+
+
 def LLI(rows_number): return [LI() for _ in range(rows_number)]
 
 
 def solver(inputNum):
     result = "No"
     # algorithm
+    strNum = str(inputNum)
+    divisorNum = 0
+    for j in range(0, len(strNum), +1):
+        divisorNum = divisorNum + int(strNum[j])
+    if inputNum % divisorNum == 0:
+        result = "Yes"
     return result
 
 
