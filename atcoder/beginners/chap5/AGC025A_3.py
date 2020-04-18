683a97421331360150383006847a3ce238d5cb1a
Yamanaka Kohji <ymnkkj@gmail.com>
2020-04-18 19:26:01 +0900

    AGC025A 3rd Start 202004181925

---
+++ atcoder/beginners/chap5/AGC025A_3.py
@@ -0,0 +1,29 @@
+# Problem: https://atcoder.jp/contests/agc025/tasks/agc025_a
+# Python 3rd Try
+
+import sys
+# from collections import defaultdict
+# import heapq,copy
+# from collections import deque
+
+
+def II(): return int(sys.stdin.readline())
+
+
+def MI(): return map(int, sys.stdin.readline().split())
+
+
+def LI(): return list(map(int, sys.stdin.readline().split()))
+
+
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+
+def solver():
+    result = 0
+    # algorithm
+    return result
+
+
+if __name__ == "__main__":
+    print("{}".format(solver()))
