415b2c92cb91b7a2d61d0c51ddb0606934b220ca
Yamanaka Kohji <ymnkkj@gmail.com>
2020-04-19 19:54:23 +0900

    AGC025A 3rd Finish 202004191954

--- atcoder/beginners/chap5/AGC025A_3.py
+++ atcoder/beginners/chap5/AGC025A_3.py
@@ -19,11 +19,19 @@ def LI(): return list(map(int, sys.stdin.readline().split()))
 def LLI(rows_number): return [LI() for _ in range(rows_number)]
 
 
-def solver():
+def solver(inputNum):
     result = 0
     # algorithm
+    totalDigit = 0
+    inputStr = str(inputNum)
+    for j in range(0, len(inputStr)):
+        totalDigit = totalDigit + int(inputStr[j])
+    if totalDigit == 1:
+        result = 10
+    else:
+        result = totalDigit
     return result
 
 
 if __name__ == "__main__":
-    print("{}".format(solver()))
+    print("{}".format(solver(II())))
