1180ca2eac1b64bc64f9156ba08d6295b2075a3e
Yamanaka Kohji <ymnkkj@gmail.com>
2020-04-29 12:54:38 +0900

    ABC027A 1st Start 202004291254

---
+++ atcoder/beginners/chap6/AGC027A_1.py
@@ -0,0 +1,31 @@
+# Problem: https://atcoder.jp/contests/agc027/tasks/agc027_a
+# Python 1st Try
+
+import sys
+# from collections import defaultdict
+# import heapq,copy
+# from collections import deque
+
+
+def II(): return int(sys.stdin.readline())
+
+
+def MI(): return map(int, sys.stdin.readline().split())
+
+
+def LI(): return list(map(int, sys.stdin.readline().split()))
+
+
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+
+def solver():
+    result = 0
+    # algorithm
+    return result
+
+
+if __name__ == "__main__":
+    N, X = MI()
+    childHappyCount = LI()
+    print("{}".format(solver()))
---
+++ atcoder/beginners/chap6/AGC027A_MEMO.md
@@ -0,0 +1,8 @@
+# AGC027A-Candy Distribution Again
+[Problem](https://atcoder.jp/contests/agc027/tasks/agc027_a)
+-----
+# 1回目
+## 2020/04/29
+* きっかりで与えなければいけない
+    * 足りなくてももらいすぎでもダメ
+        * ちょっとまず解説を見てみるか
