ad50c6ba5747a2c808b1ca62615558bcea96ca77
Yamanaka Kohji <ymnkkj@gmail.com>
2020-05-14 19:43:04 +0900

    AGC012 3rd Start 202005141942

---
+++ atcoder/beginners/chap6/AGC012A_3.py
@@ -0,0 +1,21 @@
+# Problem: https://atcoder.jp/contests/agc012/tasks/agc012_a
+# Python 3rd Try
+
+import sys
+# from collections import defaultdict
+# import heapq,copy
+# from collections import deque
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+def solver(groups, eachScore):
+    result = 0
+    print("Group : {}".format(groups))
+    print("Score : {}".format(eachScore))
+    # algorithm
+    return result
+if __name__ == "__main__":
+    N = II()
+    AI = LI()
+    print("{}".format(solver(N, AI)))
