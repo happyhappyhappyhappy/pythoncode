b53ab247b4ba9ab0cdb1295dae8993c163de8b8c
Yamanaka Kohji <ymnkkj@gmail.com>
2020-06-12 19:53:10 +0900

    20200612 ABC047B Making

--- atcoder/beginners/chap7/ABC047B_1.py
+++ atcoder/beginners/chap7/ABC047B_1.py
@@ -12,22 +12,33 @@ def LI(): return list(map(int, sys.stdin.readline().split()))
 def LLI(rows_number): return [LI() for _ in range(rows_number)]
 
 
-def solver(Width, Height, x_position, y_position, control_id):
+def solver(Width, Height, Point, x_position, y_position, control_id):
     result = 0
     surface = [[0] * (Width+1) for _ in range(0, Height+1, +1)]
     # algorithm
     # pprint.pprint(surface)
-    if control_id[0] == 1:
-        for x in range(0, Width):
-            for y in range(0, Height):
-                if x < x_position[0]:
-                    surface[x][y] = 1
-    elif control_id[0] == 2:
-        pass
-    elif control_id[0] == 3:
-        pass
-    else: # control_id[X] == 4
-        pass
+    for j in range(0, Point, +1):
+        if control_id[j] == 1:
+            for x in range(0, Width+1):
+                for y in range(0, Height+1):
+                    if x <= x_position[j]:
+                        #  print("x={},y={} Point".format(x, y))
+                        surface[y][x] = 1
+        elif control_id[j] == 2:
+            for x in range(0, Width+1):
+                for y in range(0, Height+1):
+                    if x_position[j] <= x:
+                        surface[y][x] = 1
+        elif control_id[j] == 3:
+            for x in range(0, Width+1):
+                for y in range(0, Height+1):
+                    if y <= y_position[j]:
+                        surface[y][x] = 1
+        else:  # control_id[X] == 4
+            for x in range(0, Width+1):
+                for y in range(0, Height+1):
+                    if y_position[j] <= y:
+                        surface[y][x] = 1
     pprint.pprint(surface)
     return result
 
@@ -42,4 +53,4 @@ if __name__ == "__main__":
         x_position.append(x_tmp)
         y_position.append(y_tmp)
         control_id.append(a_tmp)
-    print("{}".format(solver(W, H, x_position, y_position, control_id)))
+    print("{}".format(solver(W, H, N, x_position, y_position, control_id)))
