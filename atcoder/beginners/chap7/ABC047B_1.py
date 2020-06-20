bc4c0d24894309987a92e313aba3bb7f3454db4f
Yamanaka Kohji <ymnkkj@gmail.com>
2020-06-20 16:43:20 +0900

    ABC047B 1回目 3度目の挑戦で完了 202006201643

--- atcoder/beginners/chap7/ABC047B_1.py
+++ atcoder/beginners/chap7/ABC047B_1.py
@@ -3,7 +3,7 @@
 
 import sys
 import numpy
-import pprint
+# import pprint
 # from collections import defaultdict
 # import heapq,copy
 # from collections import deque
@@ -17,38 +17,31 @@ def solver(Width, Height, Point, x_position, y_position, control_id):
     result = 0
     # surface = [[0] * (Width+1) for _ in range(0, Height+1, +1)]
     surface = numpy.zeros((Width, Height), dtype=numpy.int)
-    pprint.pprint(surface)
+    # pprint.pprint(surface)
     # algorithm
     # pprint.pprint(surface)
     for j in range(0, Point, +1):
         if control_id[j] == 1:
-            for x in range(0, Width):
-                for y in range(0, Height):
-                    if x < x_position[j]:
-                        #  print("x={},y={} Point".format(x, y))
-                        surface[x][y] = 1
+            for x in range(0, x_position[j], +1):
+                for y in range(0, Height, +1):
+                    surface[x][y] = 1
         elif control_id[j] == 2:
-            for x in range(0, Width):
-                for y in range(0, Height):
-                    if x_position[j] < x:
-                        surface[x][y] = 1
+            for x in range(x_position[j], Width, +1):
+                for y in range(0, Height, +1):
+                    surface[x][y] = 1
         elif control_id[j] == 3:
-            for x in range(0, Width):
-                for y in range(0, Height):
-                    if y < y_position[j]:
-                        surface[x][y] = 1
+            for x in range(0, Width, +1):
+                for y in range(0, y_position[j], +1):
+                    surface[x][y] = 1
         else:  # control_id[X] == 4
-            for x in range(0, Width):
-                for y in range(0, Height):
-                    if y_position[j] < y:
-                        surface[x][y] = 1
-    pprint.pprint(surface)
-    '''
-    for x in range(0, Width+1, +1):
-        for y in range(0, Height+1, +1):
-            if surface[y][x] == 0:
+            for x in range(0, Width, +1):
+                for y in range(y_position[j], Height, +1):
+                    surface[x][y] = 1
+    #  pprint.pprint(surface)
+    for x in range(0, Width, +1):
+        for y in range(0, Height, +1):
+            if surface[x][y] == 0:
                 result = result + 1
-    '''
     return result
 
 
