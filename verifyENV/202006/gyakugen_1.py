44548a921c76785a6ccdab4d1c99ea3fa289dacf
Yamanaka Kohji <ymnkkj@gmail.com>
2020-06-28 20:01:32 +0900

    逆元攻略

---
+++ verifyENV/202006/gyakugen_1.py
@@ -0,0 +1,18 @@
+# 逆元を求める　基本１
+
+# ここにフェルマーの小定理から導ける値を書く
+
+
+def powmodinv(givenNum, pow_num, modNo):
+    return 0
+
+
+def modinv(givenNum, modNo):
+
+    return 0
+
+
+if __name__ == "__main__":
+    p = 13
+    for x in range(1, p, +1):
+        modinv(x, p)
