fe7341b7dc3c24b2090a1c217e29ba98fb1c2bc4
Yamanaka Kohji <ymnkkj@gmail.com>
2020-06-28 19:52:41 +0900

    vepass

--- ve/202004/20200424/stringdel1.py
+++
@@ -1,9 +0,0 @@
-#/mingw64/bin/python3
-import sys
-
-
-if __name__ == "__main__":
-    allargv = sys.argv
-    print("argc :{}".format(allargv))
-    x = allargv[1]
-    print("x    :{}".format(r'x'))
--- ve/scrape1/scrape1.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-problem_url = "https://atcoder.jp/contests/abc085/tasks/abc085_a"
-problem_html = requests.get(problem_url)
-problem_soup = BeautifulSoup(problem_html.content, "html.parser")
-for i in problem_soup.find_all("pre"):
-    print(i.get_text())
