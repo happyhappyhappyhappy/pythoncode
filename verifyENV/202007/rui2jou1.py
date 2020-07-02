54b77692fb958efff86005f7b8813205ae7cfe2b
Yamanaka Kohji <ymnkkj@gmail.com>
2020-07-02 19:49:35 +0900

    readme

---
+++ verifyENV/202007/rui2jou1.py
@@ -0,0 +1,19 @@
+BASE = 2
+
+
+def accumu(base, j):
+    result = 1
+    a = 1
+    while 0 < j:
+        if j & 1:
+            result = base * a
+        a = base * base
+        j = j >> 1
+    return result
+
+
+if __name__ == "__main__":
+    N = int(input())
+    for j in range(0, N, +1):
+        res = accumu(BASE, j)
+        print("{} no {} jou {}".format(BASE, j, res))
