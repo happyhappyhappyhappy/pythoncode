fc5c1691992bcfcf7c2d7d4f252972934fe67820
Yamanaka Kohji <ymnkkj@gmail.com>
2020-07-05 19:47:26 +0900

    ABC081C 3rd creating 20200705

---
+++ atcoder/beginners/chap7/ABC081C_3.py
@@ -0,0 +1,33 @@
+# Problem: https://atcoder.jp/contests/abc081/tasks/arc086_a
+# Python 3rd Try
+
+import sys
+from collections import Counter
+# import heapq,copy
+import pprint
+# from collections import deque
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+
+def solver(ball_count, limit_num, ball_write_nums):
+    result = 0
+    total_count = Counter(ball_write_nums)
+    count_list = list(total_count.values())
+    pprint.pprint("{}".format(total_count))
+    pprint.pprint("{}".format(count_list))
+    count_list.sort()
+    pprint.pprint("{}".format(count_list))
+    pprint.pprint("{}".format(ball_count-limit_num))
+    for j in range(0, ball_count-limit_num, +1):
+        result += count_list[j]
+    # algorithm
+    return result
+
+
+if __name__ == "__main__":
+    K, N = MI()
+    SI = LI()
+    print("{}".format(solver(K, N, SI)))
