7116b316a51f798e5e938a59f41406a9789cd9ab
Yamanaka Kohji <ymnkkj@gmail.com>
2020-08-04 19:50:51 +0900

    ABC085C 2nd :running: 202008041950

--- atcoder/beginners/chap8/ABC085C_2.py
+++ atcoder/beginners/chap8/ABC085C_2.py
@@ -13,8 +13,15 @@ def LLI(rows_number): return [LI() for _ in range(rows_number)]
 
 
 def solver(papers, totalSum):
-    result = [0, 0, 0] # 10000円札,5000円札,1000円札
-    # algorithm
+    result = " ".join(map(str,[ -1 , -1 , -1])) # 10000円札,5000円札,1000円札
+    for x in range(0, papers+1 , +1):
+        for y in range(papers - x, papers+1 , +1):
+            part_hundred_yen = totalSum - x*10000 - y*5000
+            print("1000Yen={}".format(part_hundred_yen))
+            if_hundred_yen_papers = part_hundred_yen // 1000
+            if 0 <=if_hundred_yen_papers and 0 <= (papers - x - y - if_hundred_yen_papers):
+                result = " ".join(map(str,[x, y, if_hundred_yen_papers]))
+                return result
     return result
 
 
