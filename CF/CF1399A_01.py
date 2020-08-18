e81966a7158df12e09c5045d3fe4486b14d2c847
Yamanaka Kohji <ymnkkj@gmail.com>
2020-08-18 21:39:06 +0900

    error.txt

---
+++ CF/CF1399A_01.py
@@ -0,0 +1,27 @@
+# Problem: https://codeforces.com/contest/1399/problem/A
+# Python 1st Try
+
+import sys
+# from collections import defaultdict
+# import heapq,copy
+import pprint as pp
+# from collections import deque
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+
+# Const
+MAXSIZE = ( 1 << 31 ) -1
+MINSIZE = -( 1 << 31) + 1
+
+
+def solver():
+    result = 0
+    # algorithm
+    return result
+
+
+if __name__ == "__main__":
+    print("{}".format(solver()))
