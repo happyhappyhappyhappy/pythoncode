e497071a0678401b6f495d4208595ed61e080141
Yamanaka Kohji <ymnkkj@gmail.com>
2020-08-23 19:54:22 +0900

    環境作成 202008231954

---
+++ atcoder/beginners/chap8/ABC085C_3.py
@@ -0,0 +1,28 @@
+# Problem: https://atcoder.jp/contests/abc085/tasks/abc085_c
+# Python 3rd Try
+
+import sys
+# from collections import defaultdict
+# import heapq,copy
+import pprint as pp
+# from collections import deque
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+
+# Const
+MAXSIZE = ( 1 << 31 ) -1
+MINSIZE = -( 1 << 31) + 1
+YES = "YES"
+NO = "NO"
+
+def solver():
+    result = 0
+    # algorithm
+    return result
+
+
+if __name__ == "__main__":
+    print("{}".format(solver()))
