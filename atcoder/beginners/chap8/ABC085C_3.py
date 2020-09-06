fd54f11c207c157ff074dd64b855714bbde741d9
Yamanaka Kohji <ymnkkj@gmail.com>
2020-09-06 19:50:07 +0900

    ABC085C 3rd End :heartbeat: ABC088C 3rd :eyes: 202009061950

--- atcoder/beginners/chap8/ABC085C_3.py
+++ atcoder/beginners/chap8/ABC085C_3.py
@@ -18,11 +18,19 @@ MINSIZE = -( 1 << 31) + 1
 YES = "YES"
 NO = "NO"
 
-def solver():
-    result = 0
+def solver(totalSheet, sum):
+    result = [-1, -1, -1]
     # algorithm
+    for sheet_10000 in range(0, totalSheet+1, +1):
+        for sheet_5000 in range(0, totalSheet + 1 - sheet_10000, +1):
+            sheet_1000 = totalSheet - sheet_10000 - sheet_5000
+            nowSum = sheet_10000 * 10000 + sheet_5000 * 5000 + sheet_1000 * 1000
+            if nowSum == sum:
+                result = [sheet_10000 ,sheet_5000, sheet_1000]
     return result
 
 
 if __name__ == "__main__":
-    print("{}".format(solver()))
+    N, Y = MI()
+    answer = solver(N, Y)
+    print("{} {} {}".format(answer[0], answer[1], answer[2]))
