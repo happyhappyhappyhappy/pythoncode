0f29d7820e0c5e33d3375427c446590e5b54742a
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-11-27 19:24:44 +0900

    とりあえず着手開始

---
+++ atcoder/mizuiro/nibuntansaku/ALDS4Bnibuntansaku/README.md
@@ -0,0 +1,15 @@
+# \[ALDS_4_B\]\<二分探索\>
+
+## [問題のURL](https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_4_B)
+
+## [解説のURL](https://onlinejudge.u-aizu.ac.jp/resources/commentaries/ALDS1_4_B/ja/post?general=Algorithm)
+
+---
+
+ヒント
+
+| 回数 | 達成日 | 言語 |
+| --- | ----- | ---- |
+| 1回目 |  |  |
+| 2回目 |  |  |
+| 3回目 |  |  |
---
+++ atcoder/mizuiro/nibuntansaku/ALDS4Bnibuntansaku/first/submit1.py
@@ -0,0 +1,28 @@
+import sys
+# from collections import defaultdict
+# import heapq,copy
+import pprint as pp
+# from collections import deque
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+
+# Const
+MAXSIZE = ( 1 << 31 ) -1
+MINSIZE = -( 1 << 31) + 1
+
+
+def solver():
+    result = 0
+    # algorithm
+    return result
+
+
+if __name__ == "__main__":
+    n = int(input())
+    S = LI()
+    q = II()
+    T = LI()
+    print("{}".format(solver()))
