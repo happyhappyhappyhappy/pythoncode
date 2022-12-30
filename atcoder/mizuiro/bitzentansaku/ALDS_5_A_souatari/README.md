f53b425296e78346518877878b6a62ea3289e769
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-12-30 19:23:43 +0900

    途中まで進めてみた

---
+++ atcoder/mizuiro/bitzentansaku/ALDS_5_A_souatari/README.md
@@ -0,0 +1,15 @@
+# \[ALDS_5_A\]\<問題名\>
+
+## [問題のURL](http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_5_A&lang=ja)
+
+## [解説のURL](https://onlinejudge.u-aizu.ac.jp/resources/commentaries/ALDS1_5_A/ja/post?general=Algorithm)
+
+---
+
+ヒント
+
+| 回数 | 達成日 | 言語 |
+| --- | ----- | ---- |
+| 1回目 |  |  |
+| 2回目 |  |  |
+| 3回目 |  |  |
--- atcoder/mizuiro/bitzentansaku/ALDS_5_A_souatari/submit1.py
+++ atcoder/mizuiro/bitzentansaku/ALDS_5_A_souatari/submit1.py
@@ -15,16 +15,26 @@ MAXSIZE = ( 1 << 31 ) -1
 MINSIZE = -( 1 << 31) + 1
 
 
-def solver(gList,sList):
-    gLen = len(gList)
-    sLen = len(sList)
-    print("与えられた数")
-    for x in range(gLen):
-        print("{} -> {}".format(x,gList[x]))
-    print("作る数")
-    for x in range(sLen):
-        print("{} -> {}".format(x,sList[x]))
-    result = 0
+def dfs(pos,all_pos,balance,g_List):
+    print("現在の位置 : {}   全部の数 : {}".format(pos,all_pos))
+    print("余りの値 : {}".format(balance))
+    print("与えられたリスト : {}".format(g_List))
+    if balance == 0:
+        print("めでたくぴったりの値になった")
+        return True
+    if balance < 0 :
+        # TODO: ここの出力部分を書くところからスタート
+        return False
+    if pos == all_pos :
+        print("")
+        return False
+
+
+    return dfs(pos+1,all_pos,balance-g_List[pos],g_List) or dfs(pos+1,all_pos,balance,g_List)
+
+def solver(gList,i_sum):
+    print("List {} から {} が出来るか確認する".format(gList,i_sum))
+    result = dfs(0,len(gList),i_sum,gList)
     # algorithm
 
     return result
@@ -35,6 +45,8 @@ if __name__ == "__main__":
     noList = [ int(x) for x in (input2().split())]
     solveNum = II()
     solveList = [ int(x) for x in (input2().split())]
-    # for j in range(givenNum):
-    #    print("{} -> {}".format(j,noList[j]))
-    print("{}".format(solver(noList,solveList)))
+    for x in range(solveNum):
+        if solver(noList,solveList[x]):
+            print("yes")
+        else:
+            print("no")
