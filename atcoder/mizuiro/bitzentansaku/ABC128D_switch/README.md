ee41e2ef639b608ad7968fb7730c44c16ff4d1e6
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-01-07 19:37:20 +0900

    うっかりしていた

---
+++ atcoder/mizuiro/bitzentansaku/ABC128D_switch/README.md
@@ -0,0 +1,9 @@
+# \[ABC128D\]\<switch\>
+
+## [問題のURL](https://atcoder.jp/contests/abc128/tasks/abc128_c)
+
+## [解説のURL](https://blog.hamayanhamayan.com/entry/2019/06/03/202146)
+
+## [この問題の結果](https://atcoder.jp/contests/abc128/submissions?f.Task=abc128_c&f.LanguageName=Python3&f.Status=AC&f.User=)
+
+## 回答実績
--- atcoder/mizuiro/bitzentansaku/ABC128D_switch/submit1.py
+++ atcoder/mizuiro/bitzentansaku/ABC128D_switch/submit1.py
@@ -1,2 +1,22 @@
-testdir
+import sys
+import pprint as pp
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
 
+S,L = MI()
+LtoS = []
+for x in range(L):
+    LtoS.append(LI())
+tx,ty = MI()
+for x in range(L):
+    print(LtoS[x])
+
+for y in range(2**S):
+    print("---{}の場合---".format(y))
+    for z in range(S):
+        if (y>>z) & 1:
+            print("{}のスイッチ押された".format(x))
+        else:
+            print("{}のスイッチ押されてない".format(x))
