f811f17df23dbb6129fd52a42999cf5b65ccf492
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-01-08 16:23:40 +0900

    まずある程度まで追加

--- atcoder/mizuiro/bitzentansaku/ABC128D_switch/submit1.py
+++ atcoder/mizuiro/bitzentansaku/ABC128D_switch/submit1.py
@@ -1,22 +1,69 @@
 import sys
+# from collections import defaultdict
+# import heapq,copy
 import pprint as pp
+# from collections import deque
 def II(): return int(sys.stdin.readline())
 def MI(): return map(int, sys.stdin.readline().split())
 def LI(): return list(map(int, sys.stdin.readline().split()))
 def LLI(rows_number): return [LI() for _ in range(rows_number)]
 
-S,L = MI()
-LtoS = []
-for x in range(L):
-    LtoS.append(LI())
-tx,ty = MI()
-for x in range(L):
-    print(LtoS[x])
 
-for y in range(2**S):
-    print("---{}の場合---".format(y))
-    for z in range(S):
-        if (y>>z) & 1:
-            print("{}のスイッチ押された".format(x))
-        else:
-            print("{}のスイッチ押されてない".format(x))
+# Const
+MAXSIZE = ( 1 << 31 ) -1
+MINSIZE = -( 1 << 31) + 1
+
+def solver(Switch,Lamp,G,Echeck):
+
+    for sw_pat in range(1<<Switch):
+        swlist = list()
+        pushedSwitch = 0
+        for shift in range(Switch):
+            if ( sw_pat >> shift) & 1:
+                swlist.append(1)
+                pushedSwitch = pushedSwitch+1
+            else:
+                swlist.append(0)
+        checkOddEven = pushedSwitch % 2
+        print("{} の場合の 押されるスイッチ列 {}".format(sw_pat,swlist))
+        print("偶奇合わせ : {}".format(checkOddEven))
+        # Lamp毎のチェック
+        lamp_counter=0
+        for lamps in range(Lamp):
+            check_counter=0
+            use_switch = len(grid[lamps])
+            for x in range(use_switch):
+                # 各ランプで使うスイッチが今ONになっているか確認する
+    # for nowlamp in range(Lamp)
+    #     for x in range(2**Switch):
+    #         print("bit列 {} の場合".format(x))
+    #         sw_count = 0
+    #         for shift in range(Switch):
+    #             if (x >> shift) & 1:
+    #                 sw_count = sw_count+1
+    #             print("  スイッチ {} は押されます".format(shift))
+    #             else:
+    #             print("   スイッチ {} は押されません".format(shift))
+    #         print("スイッチ計: {}".format(sw_count))
+    result = 0
+    # algorithm
+    return result
+
+
+if __name__ == "__main__":
+    # 情報入力
+    N,M = MI()
+    grid=list()
+    for x in range(M):
+        grid.append(LI())
+    even_or_odd = LI()
+    # ざっくりと挿入したgridに手を加える
+    # 1.各行の1つめは削除する
+    for x in range(M):
+        grid[x].pop(0)
+    # 2.0-indexへ修正する
+    for x in range(M):
+        y = len(grid[x])
+        for z in range(y):
+            grid[x][z] = grid[x][z]-1
+    print("{}".format(solver(N,M,grid,even_or_odd)))
