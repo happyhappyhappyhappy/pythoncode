aa71beb6937e7e19c33cfaa2f00961f40a3f4e1b
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-01-18 19:42:28 +0900

    少しCounterオブジェクトを付けてみた

--- atcoder/mizuiro/bitzentansaku/ABC128D_switch/submit1.py
+++ atcoder/mizuiro/bitzentansaku/ABC128D_switch/submit1.py
@@ -1,5 +1,5 @@
 import sys
-# from collections import defaultdict
+from collections import Counter
 # import heapq,copy
 from logging import getLogger, StreamHandler, DEBUG
 # import pprint as pp
@@ -43,12 +43,18 @@ for pat in range(2**N):
     # logger.debug("{} -> {}".format(pat,bin(pat)))
     all_lamp_on=True
     for j in range(M):
+        tso_list = list()
         for pos in range(len(G[j])):
             total_switch_ok=0
-            logger.debug("pat = {},checkpos ={}".format(pat,bin(pat>>G[j][pos])))
+            logger.debug("\tpat = {},checkpos ={}".format(pat,bin(pat>>G[j][pos])))
             if ((pat>>G[j][pos]) & 1) == 1:
                 logger.debug("この時付く")
                 total_switch_ok=total_switch_ok+1
+                tso_list.append(True)
+            else:
+                tso_list.append(False)
+        x = Counter(tso_list)
+        logger.debug("\tランプ {} のスイッチ一致状況は {}".format(j,x))
         logger.debug("奇遇数: {}".format(Odd))
         if Odd[j] != (total_switch_ok%2):
             logger.debug("ランプ {} は 奇遇数が一致しないので付かない".format(j))
