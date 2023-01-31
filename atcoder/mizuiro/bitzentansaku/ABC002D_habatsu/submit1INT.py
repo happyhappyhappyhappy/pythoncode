32379cb14eccf6a563478b0c264a471ca38537b1
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-01-31 19:38:26 +0900

    新規作成 一旦終了

---
+++ atcoder/mizuiro/bitzentansaku/ABC002D_habatsu/submit1INT.py
@@ -0,0 +1,64 @@
+import sys
+# from collections import defaultdict
+# import heapq,copy
+from logging import getLogger, StreamHandler, DEBUG
+import pprint as pp
+# from collections import deque
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+xdebug=logger.debug
+pppp=pp.pprint
+
+N,M = MI()
+# xdebug("N={} M={}".format(N,M))
+G = [ [0] * N for _ in range(N)]
+# pppp(G)
+for _ in range(M):
+    x,y = MI()
+    x = x-1
+    y = y-1
+    # xdebug("x={},y={}".format(x,y))
+    G[x][y]=1
+    G[y][x]=1
+for x in range(N):
+    G[x-1][x-1]=1 # 自分自身に対しては一応1
+# pppp(G)
+
+# bit全探索
+
+ans = 1
+for bit_p in range(1,1<<N):
+    chainList=list()
+    for j in range(N):
+        if ((bit_p>>j) & 1) == 1:
+            chainList.append(j)
+    # xdebug("派閥リスト: {}".format(chainList))
+    is_ok=1
+    if len(chainList) == 1:
+        # xdebug("\t一人では派閥は作れません") # 一人で派閥は作れない -> 即時に次のループへ
+        continue
+    chainLen = len(chainList)
+    for f in range(chainLen):
+        for t in range(f+1,chainLen):
+            f_pos = chainList[f]
+            t_pos = chainList[t]
+            if G[f_pos][t_pos] == 0 or G[t_pos][f_pos] ==0 :
+                # xdebug("\t{} と {} が知り合いではありません" \
+                #    .format(f_pos,t_pos))
+                is_ok = 0
+    if is_ok == 1 :
+        # xdebug("\t{}は全部揃いました".format(chainList))
+        if ans < chainLen:
+            # xdebug("\t\t現在の回答より長いので置き換えます")
+            ans = chainLen
+
+print("{}".format(ans))
