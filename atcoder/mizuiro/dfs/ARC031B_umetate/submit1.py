64587abb1a0c4f3a882e4717211d73ea519c4b82
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-03-14 19:01:49 +0900

    Commit message: Add copy for deepcopy and isgo function for valid checking

--- atcoder/mizuiro/dfs/ARC031B_umetate/submit1.py
+++ atcoder/mizuiro/dfs/ARC031B_umetate/submit1.py
@@ -3,6 +3,7 @@ import sys
 # from collections import defaultdict
 # import heapq,copy
 import pprint as pp
+import copy
 from collections import deque
 from logging import getLogger, StreamHandler, DEBUG
 
@@ -27,12 +28,30 @@ ppp=pp.pprint
 MAXSIZE = ( 1 << 59 ) -1
 MINSIZE = -( 1 << 59) + 1
 
+def isgo(G,posh,posw):
+    # TODO: ここをコーディング
+    # posh,posw 両方0未満->X
+    # posh,posw 両方10以上->X
+    # 海である
+    # return false
+    return true
+
 def solver(G):
     result = "NO"
+
     for h in range(0,10):
         for w in range(0,10):
             if G[h][w] == 'x':
-                G[h][w] = 'v' # 探索済みを入れてしまう
+                G_c=copy.deepcopy(G)
+                G_c[h][w] = 'v' # 探索済みを入れてしまう
+                dh = [1,0,-1,0]
+                dw = [0,1,0,-1]
+                dq = deque([h,w])
+                while len(dq) != 0:
+                    [posh,posw] = dq.pop()
+                    for x in range(0,4):
+                        next_h = posh + dh[x]
+                        next_w = posw + dw[x]
 
                 break
 
