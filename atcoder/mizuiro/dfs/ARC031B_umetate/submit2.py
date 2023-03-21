63d56669a5a14dabe8ff90035e6906773e127947
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-03-21 16:47:46 +0900

    Refactor code and add boundary wall to solve edge cases

--- atcoder/mizuiro/dfs/ARC031B_umetate/submit2.py
+++ atcoder/mizuiro/dfs/ARC031B_umetate/submit2.py
@@ -32,10 +32,12 @@ def showG(G,R,W):
     for r in range(0,R):
         xdebug(G[r])
 
-def isntGo(G,r,w):
-    if r < 0 or w < 0 or 10 <= r or 10 <= w:
-        return true
-    if G[r][w] != # TODO: ここから 経過時間 18:25
+def isnotGo(G,r,w):
+    c_value=G[r][w]
+    if c_value == 'T' or c_value == 'x' or c_value == 'v':
+        return 1
+    else:
+        return 0
 
 def dfs(G,sea_r,sea_w):
     flug = False
@@ -43,21 +45,40 @@ def dfs(G,sea_r,sea_w):
     dh = [1,0,-1,0]
     dw = [0,1,0,-1]
     G[sea_r][sea_w] = 'v'
-    mystack.append(list(sea_r,sea_w))
+    mystack.append([sea_r,sea_w])
     while len(mystack) != 0:
         nowpos = mystack.pop()
         for t in range(0,4):
-
+            nexth = nowpos[0]+dh[t]
+            nextw = nowpos[0]+dw[t]
+            if isnotGo(G,nexth,nextw) == 1:
+                continue
+            else:
+                G[nexth][nextw]='v'
+                mystack.append([nexth,nextw])
+    # 一通り'v'付けられるだけ付けてみた'o'がどれだけ付いたか確認
+    for r in range(len(G)):
+        for w in range(len(G[0])):
+            if G[r][w] == 'o':
+                # 失敗
+                return 0
+    # 成功
+    return 1
 
 def solver(G,R,W):
     flug = False
+    # 周辺の柵を作る
+    GS=[[ 'T' for _ in range(W+2)] for _ in range(R+2)]
     for r in range(0,R):
         for w in range(0,W):
-            if G[r][w] == 'x':
-                GD = cp.deepcopy(G)
+            GS[r+1][w+1] = G[r][w]
+    for r in range(0,R+2):
+        for w in range(0,W+2):
+            if GS[r][w] == 'x':
+                GD = cp.deepcopy(GS)
                 flug = dfs(GD,r,w)
     # algorithm
-    if flug:
+    if flug == 1:
         return "YES"
     else:
         return "NO"
@@ -68,5 +89,5 @@ if __name__ == "__main__":
     for _ in range(10):
         ltmp = list(input())
         G.append(ltmp)
-    showG(G,10,10)
+    # showG(G,10,10)
     print("{}".format(solver(G,len(G),len(G[0]))))
