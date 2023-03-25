6fa2aa9de4a00f800d83a3458d422aef52402e8c
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-03-25 19:28:20 +0900

    問題の追加をしました

---
+++ atcoder/mizuiro/bfs_habayusentansaku/JOI2011YOSEN_E_illmination/sample/sample.py
@@ -0,0 +1,71 @@
+# ライブラリのインポート
+import sys
+import copy
+import pprint as pp
+from collections import deque
+import queue # 出来るのか？？
+# pypy3用
+import pypyjit
+# 再帰制御解放
+pypyjit.set_param('max_unroll_recursion=-1')
+sys.setrecursionlimit(10**6)
+from logging import getLogger, StreamHandler, DEBUG
+
+# 入力のマクロ
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+# デバッグ出力の作成
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+# クラス+メソッドを一関数
+xdebug=logger.debug
+ppp=pp.pprint
+# Const
+MAXSIZE = ( 1 << 59 ) -1
+MINSIZE = -( 1 << 59) + 1
+
+dwdh_odd=[
+    (1,-1), # 0:00-2:00
+    (1,0), # 2:00-4:00
+    (1,1), # 4:00-6:00
+    (0,1), # 6:00-8:00
+    (-1,0), # 8:00-10:00
+    (0,-1) # 10:00-12:00
+]
+dwdh_even=[
+    (0,-1), # 0:00-2:00
+    (1,0), # 2:00-4:00
+    (1,1), # 4:00-6:00
+    (-1,1), # 6:00-8:00
+    (-1,0), # 8:00-10:00
+    (-1,-1) # 10:00-12:00
+]
+
+def solver():
+    result = 0
+    # algorithm
+    return result
+
+def ShowG(G,H):
+    for h in range(0,H):
+        xdebug(G[h])
+
+if __name__ == "__main__":
+    W,H=map(int,input().split())
+    G=[]
+    G.append([0]*(W+2))
+    for h in range(0,H):
+        S = list(LI())
+        xdebug("{} 行目に {} を入れたい".format(h,S))
+        G.append([0]+S+[0])
+    G.append([0]*(W+2))
+    ShowG(G,H+2)
+    print("{}".format(solver()))
