8a813a2d72ab1914f3b0964aed82925b76f957aa
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-06-09 16:03:16 +0900

    Find minimum value of Ans

--- atcoder/mizuiro_h20/bitzentansaku/ABC197C_ORXOR/sample1.py
+++ atcoder/mizuiro_h20/bitzentansaku/ABC197C_ORXOR/sample1.py
@@ -34,3 +34,19 @@ MINSIZE = -( 1 << 59) + 1
 # https://atcoder.jp/contests/abc197/submissions/21295839
 # そのまま-> mainを用いない物 TODO:続き
 # 2023-06-08 19:33:07
+
+N = int(input())
+A=list(map(int,input().split()))
+ANS = MAXSIZE
+def f(m1,m2,i):
+    global ANS
+    if i==N:
+        ANS=min(ANS,c^x)
+    else:
+        xdebug("A[{}]")
+        f(A[i],c^x,i+1)
+        f(c|A[i],x,i+1)
+
+# ここにあれこれ書く
+f(0,0,0)
+print(ANS)
