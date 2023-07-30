b7b14269aded4f15eeb67cb491004e364312d4a8
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-07-30 16:41:52 +0900

    復習

---
+++ atcoder/mizuiro_h20/fukasayusentansaku/pana2020Dstringequibalance/second_rev/submit2rd.py
@@ -0,0 +1,48 @@
+# ライブラリのインポート
+import sys
+# import heapq,copy
+import pprint as pp
+# from collections import deque
+# pypy3用
+import pypyjit
+# 再帰制御解放
+pypyjit.set_param('max_unroll_recursion=-1')
+sys.setrecursionlimit(10**6)
+from logging import getLogger, StreamHandler, DEBUG
+
+# 入力のマクロ
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+# デバッグ出力の作成
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+# クラス+メソッドを一関数
+xdebug=logger.debug
+ppp=pp.pprint
+# Const
+MAXSIZE = ( 1 << 59 ) -1
+MINSIZE = -( 1 << 59) + 1
+
+N = II()
+
+def dfs(L):
+    if len(L)==N:
+        S="".join(L)
+        print(S)
+        return
+    maxChar = max(L)
+    for j in range(ord('a'),ord(maxChar)+2):
+        L.append(chr(j))
+        dfs(L)
+        L.pop()
+    return
+
+dfs(['a'])
