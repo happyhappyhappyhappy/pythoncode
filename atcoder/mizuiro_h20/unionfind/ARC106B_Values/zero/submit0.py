011e6035400d22d10358d19cef2c56b276c765a7
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-08-07 19:33:16 +0900

    Add logic to check if bList has any non-zero elements

---
+++ atcoder/mizuiro_h20/unionfind/ARC106B_Values/zero/submit0.py
@@ -0,0 +1,97 @@
+# ライブラリのインポート
+import sys
+# import heapq,copy
+import pprint as pp
+from collections import defaultdict
+# pypy3用
+# import pypyjit
+# 再帰制御解放
+# pypyjit.set_param('max_unroll_recursion=-1')
+# sys.setrecursionlimit(10**6)
+from logging import getLogger, StreamHandler, DEBUG
+
+# 入力のマクロ
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+# デバッグ出力の作成
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+# クラス+メソッドを一関数
+xdebug=logger.debug
+ppp=pp.pprint
+# Const
+MAXSIZE = ( 1 << 59 ) -1
+MINSIZE = -( 1 << 59) + 1
+
+# UnionFindクラスの作成
+class UnionFind():
+    def __init__(self,n):
+        self.n = n
+        self.parents=[-1]*n
+    def find(self,x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            upparents = self.parents[x]
+            self.parents[x]=self.find(upparents)
+            return self.parents[x]
+    def union(self,x,y):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return
+        if self.parents[y] < self.parents[x]:
+            x,y = y,x
+        self.parents[x]=self.parents[x]+self.parents[y]
+        self.parents[y]=x
+    def size(self,x):
+        nowRoot = self.find(x)
+        res_size = (-1)*(self.parents[nowRoot])
+        return res_size
+    def same(self,x,y):
+        ok = (self.find(x)==self.find(y))
+        return ok
+    def members(self,x):
+        root = self.find(x)
+        return [j for j in range(0,self.n) if self.find(j) == root]
+    def roots(self):
+        return [j for j,x in enumerate(self.parents)]
+    def group_count(self):
+        return len(self.roots())
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(0,self.n):
+            memspar=self.find(member)
+            group_members[memspar].append(member)
+        return group_members
+    def __str__(self):
+        res="\n".join(f"{r}: {m}" for r,m in self.all_group_members().items())
+        return res
+
+N,M = MI()
+A = LI()
+B = LI()
+uf = UnionFind(N)
+for j in range(0,M):
+    cp1,dp1 = MI()
+    c = cp1-1
+    d = dp1-1
+    uf.union(c,d)
+print(uf)
+bList = [0]*N
+for j in range(0,N):
+    x = uf.find(j)
+    bList[x]=bList[x]+(A[j]-B[j])
+ok = True
+for j in range(0,N):
+    if bList != 0:
+        # TODO: もしbListに0以外が入っていたらNo,そのまま行ったらYesを出す
+        # 2023-08-07 19:32:48
