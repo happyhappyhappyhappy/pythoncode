8bd997bc2ef83118a985bcbec6296c04fa09b82e
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-08-27 15:34:10 +0900

    Add UnionFind class methods for size, same, members, roots, group_count, and all_group_members

--- atcoder/mizuiro_h20/unionfind/ARC111B_ReversibleCards/used/copy.py
+++ atcoder/mizuiro_h20/unionfind/ARC111B_ReversibleCards/used/copy.py
@@ -4,7 +4,7 @@ class UnionFind():
         self.n=n
         self.parents=[-1]*n
     def find(self,x):
-        if parents[x]<0:
+        if self.parents[x]<0:
             return x
         else:
             self.parents[x]=self.find(self.parents[x])
@@ -16,5 +16,57 @@ class UnionFind():
             return
         if self.parents[y]<self.parents[x]:
             x,y = y,x
-        parents[x]=parents[x]+parents[y]
-        parents[y]=x
+        self.parents[x]=self.parents[x]+self.parents[y]
+        self.parents[y]=x
+    def size(self,x):
+        root = self.find(x)
+        res = (-1)*self.parents[root]
+        return res
+    def same(self,x,y):
+        xroot = self.find(x)
+        yroot = self.find(y)
+        ok = (xroot == yroot)
+        return ok
+    def members(self,x):
+        root = self.find(x)
+        res = [ j for j in range(0,self.n) if self.find(j) == root ]
+        return res
+    def roots(self):
+        res = [ x for x,p in enumerate(self.parents) if p < 0]
+        return res
+    def group_count(self):
+        x = self.roots()
+        return len(x)
+    def all_group_members(self):
+        group_members=defaultdict(list)
+        for member in range(0,self.n):
+            k  = self.find(member)
+            group_members[k].append(member)
+        return group_members
+    def __str__(self):
+        res = "\n".join(f"{r} : {m}" for r,m in self.all_group_members().items())
+        return res
+N = int(input())
+MAXCOL=400000
+CNT = [0]*MAXCOL
+uf = UnionFind(MAXCOL)
+for j in range(0,N):
+    a,b = map(int,input().split())
+    a = a-1
+    b = b-1
+    uf.union(a,b)
+    CNT[a]=CNT[a]+1
+    CNT[b]=CNT[b]+1
+ans = 0
+for group in uf.all_group_members().values():
+    cnt  = 0
+    for x in group:
+        cnt=cnt+CNT[x]
+    if cnt==0:
+        continue
+    leng=len(group)
+    if cnt == (leng-1)*2:
+        ans = ans + (leng-1)
+    else:
+        ans = ans + leng
+print(ans)
---
+++ atcoder/mizuiro_h20/unionfind/ARC111B_ReversibleCards/used/prac.py
@@ -0,0 +1,130 @@
+# ライブラリのインポート
+import sys
+# import heapq,copy
+import pprint as pp
+from collections import defaultdict
+# pypy3用
+# import pypyjit
+# 再帰制御解放
+# pypyjit.set_param('max_unroll_recursion=-1')
+# sys.setrecursionlimit(10**6)
+from logging import getLogger, StreamHandler, DEBUG
+
+# 入力のマクロ
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+# デバッグ出力の作成
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+# クラス+メソッドを一関数
+xdebug=logger.debug
+ppp=pp.pprint
+# Const
+MAXSIZE = ( 1 << 59 ) -1
+MINSIZE = -( 1 << 59) + 1
+
+class UnionFind():
+    def __init__(self,n):
+        self.n=n
+        self.parents=[-1]*n
+    def find(self,x):
+        if self.parents[x]<0:
+            return x
+        else:
+            xvalue = self.parents[x]
+            xroot = self.find(xvalue)
+            self.parents[x]=xroot
+            return xroot
+    def union(self,x,y):
+        # x->xroot,y->yrootに替えてある
+        xroot = self.find(x)
+        yroot = self.find(y)
+        if xroot == yroot:
+            return
+        if self.parents[yroot]<self.parents[xroot]:
+            xroot,yroot = yroot,xroot
+        self.parents[xroot]=self.parents[xroot]+self.parents[yroot]
+        self.parents[yroot]=xroot
+    def size(self,x):
+        root = self.find(x)
+        res = (-1)*self.parents[root]
+        return res
+    def same(self,x,y):
+        xroot = self.find(x)
+        yroot = self.find(y)
+        ok = (xroot == yroot)
+        return ok
+    def members(self,x):
+        root = self.find(x)
+        res = [ j for j in range(0,self.n) if self.find(j) == root ]
+        return res
+    def roots(self):
+        res = [ x for x,p in enumerate(self.parents) if p < 0]
+        return res
+    def group_count(self):
+        x = self.roots()
+        return len(x)
+    def all_group_members(self):
+        group_members=defaultdict(list)
+        for member in range(0,self.n):
+            mroot = self.find(member)
+            group_members[mroot].append(member)
+        return group_members
+    def __str__(self):
+        res = "\n".join(f"{r} : {m}" for r,m in self.all_group_members().items())
+        return res
+# 確認用
+# uf = UnionFind(7)
+# xdebug("-----")
+# xdebug(uf)
+# uf.union(2,3)
+# xdebug("-----")
+# xdebug(uf)
+# uf.union(2,4)
+# xdebug("-----")
+# xdebug(uf)
+# uf.union(3,4)
+# xdebug("-----")
+# xdebug(uf)
+# x=3
+# xdebug(f"uf.find({x})={uf.find(x)}")
+N = II()
+# MAXCOL=400000
+MAXCOL = 10
+CNT = [0]*MAXCOL
+uf = UnionFind(MAXCOL)
+for j in range(0,N):
+    a,b=MI()
+    a=a-1
+    b=b-1
+    uf.union(a,b)
+    CNT[a]=CNT[a]+1
+    CNT[b]=CNT[b]+1
+ans = 0
+xdebug(f"{CNT}")
+for group in uf.all_group_members().values():
+    xdebug(f"group={group}")
+    cnt = 0
+    for x in group:
+        cnt=cnt+CNT[x]
+    if cnt == 0:
+        continue
+    xdebug(f"{group} のcnt={cnt}")
+    leng=len(group)
+    if cnt == (leng-1)*2:
+        xdebug("cnt==(leng-1)*2")
+        xdebug(f"len({group}) の結果から {leng-1} を加える")
+        ans = ans+(leng-1)
+    else:
+        xdebug("cnt!=(leng-1)*2")
+        xdebug(f"len({group}) の結果から {leng} を加える")
+        ans=ans+leng
+print(ans)
