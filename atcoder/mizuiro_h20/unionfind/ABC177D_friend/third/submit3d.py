536ca4db8600ccd7a40df1b41313b2128ad6ee87
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-09-02 19:41:37 +0900

    一部追加

--- atcoder/mizuiro_h20/unionfind/ABC177D_friend/third/submit3d.py
+++ atcoder/mizuiro_h20/unionfind/ABC177D_friend/third/submit3d.py
@@ -2,7 +2,7 @@
 import sys
 # import heapq,copy
 import pprint as pp
-# from collections import deque
+from collections import defaultdict
 # pypy3用
 # import pypyjit
 # 再帰制御解放
@@ -31,14 +31,33 @@ ppp=pp.pprint
 MAXSIZE = ( 1 << 59 ) -1
 MINSIZE = -( 1 << 59) + 1
 
+
+class UnionFind():
+    def __init__(self,n):
+            self.n=n
+            self.parents=[-1]*n
+    def find(self,x):
+        if self.parents[x]<0:
+            return x
+        else:
+            self.parents[x]=self.find(self.parents[x])
+            return self.parents[x]
+    def union(self,x,y):
+        xroot = self.find(x)
+        yroot = self.find(y)
+        ok = (xroot == yroot)
+        if ok==True:
+            return
+        if self.parents[yroot]<self.parents[xroot]:
+            roottmp = xroot
+            xroot = yroot
+            yroot = roottmp
+        self.parents[xroot]=self.parents[xroot]+self.parents[yroot]
+        self.parents[yroot]=x
 # UnionFind実装
 # 2023-09-01 19:32:36
 # TODO: 次のサイトを参考の上実装
 # https://note.nkmk.me/python-union-find/
-class UnionFind():
-    __init__(self,n):
-        self.n=n
-        self.parents=[-1]*n
 
 
 
