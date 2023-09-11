a3c4a0456d616c26e22c30204b4c2ded2aed8600
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-09-11 19:24:16 +0900

    Add code for separating solved and unsolved problems based on the value of `bit`.

---
+++ atcoder/mizuiro_h20/bitzentansaku/ABC104CALLGREEN/third/submit3d.py
@@ -0,0 +1,44 @@
+# ライブラリのインポート
+import sys
+# import heapq,copy
+import pprint as pp
+# from collections import deque
+# pypy3用
+# import pypyjit
+# 再帰制御解放
+# pypyjit.set_param('max_unroll_recursion=-1')
+# sys.setrecursionlimit(10**6)
+from logging import getLogger, StreamHandler, DEBUG
+
+# 入力のマクロ
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+# デバッグ出力の作成
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+# クラス+メソッドを一関数
+xdebug=logger.debug
+ppp=pp.pprint
+# Const
+MAXSIZE = ( 1 << 59 ) -1
+MINSIZE = -( 1 << 59) + 1
+
+D,G = MI()
+BaseData=[]
+for _ in range(0,D):
+    a,b = MI()
+    BaseData.append([a,b])
+
+for bit in range(1,1<<N):
+    passP = []
+    notpassP = []
+    # 2023-09-11 19:21:40 この辺で終了
+    # TODO:bitの値に応じて今全問解いた問題、今は解かなかった問題に分離
