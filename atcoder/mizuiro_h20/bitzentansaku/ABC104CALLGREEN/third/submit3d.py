a28d84c6ad4ffe9c4f497ac94959a40dcdf63003
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-09-12 19:37:13 +0900

    Refactor code to separate solving problems and unsolved problems based on the value of `bit`. Calculate the total score and the number of solved problems for the solved problems. Check if the total score meets the passing score. If it does, update the minimum number of problems solved

--- atcoder/mizuiro_h20/bitzentansaku/ABC104CALLGREEN/third/submit3d.py
+++ atcoder/mizuiro_h20/bitzentansaku/ABC104CALLGREEN/third/submit3d.py
@@ -33,12 +33,41 @@ MINSIZE = -( 1 << 59) + 1
 
 D,G = MI()
 BaseData=[]
-for _ in range(0,D):
+for j in range(0,D):
     a,b = MI()
-    BaseData.append([a,b])
-
-for bit in range(1,1<<N):
+    BaseData.append([a,(j+1)*100,b])
+# print(BaseData)
+ans = MAXSIZE
+for bit in range(0,1<<D):
     passP = []
     notpassP = []
     # 2023-09-11 19:21:40 この辺で終了
     # TODO:bitの値に応じて今全問解いた問題、今は解かなかった問題に分離
+    for j in range(0,D):
+        x = (bit >> j) & 1
+        if x == 1:
+            passP.append(j)
+        else:
+            notpassP.append(j)
+    # xdebug(f"bit={bit},全完={passP},手つかず={notpassP}")
+    nowsum = 0
+    nowpass = 0
+    # 全完完了問題の計算
+    if len(passP)!=0:
+        ps = len(passP)
+        for j in range(0,ps):
+            np = passP[j] # 今全完した問題
+            thissum = BaseData[np][0]*BaseData[np][1]+BaseData[np][2]
+            nowsum=nowsum+thissum
+            nowpass=nowpass+BaseData[np][0]
+    xdebug(f"指定問題 {passP} 全完時-> 合計得点 {nowsum} : 合計問題 {nowpass}")
+    xdebug(f"合格点{G}に満たすか")
+    if G <= nowsum:
+        xdebug(f"{G} <= 全完 {passP}の時はここで終了 飛ばします")
+        if nowpass <= ans:
+            ans = nowpass
+            xdebug(f"問題数 {ans} になりました")
+        continue
+    else:
+        xdebug(f"補講のパターン。後で書くTODO:")
+    xdebug("continue掛かって時はここで飛ぶはず")
