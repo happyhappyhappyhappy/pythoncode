d2d7617806a68fdc9c88511cdba4d7a09016e5be
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-10-03 19:33:00 +0900

    ようやくめどが見えてきた

--- atcoder/mizuiro_h20/fukasayusentansaku/ABC119C_SystheticKadomatu/forth_zentansaku/submit4d_1.py
+++ atcoder/mizuiro_h20/fukasayusentansaku/ABC119C_SystheticKadomatu/forth_zentansaku/submit4d_1.py
@@ -42,11 +42,27 @@ xdebug(L)
 
 for j in range(0,N):
     CH[j]=4
-A_b = [] # Aに使う竹
-B_b = [] # Bに使う竹
-C_b = [] # Cに使う竹
-Z_b = [] # 使わない竹
 xdebug(CH)
+result=MAXSIZE-1
+
+def magic(A_bn,B_bn,C_bn):
+    mp=0
+    xdebug(f"Aに使う竹{A_bn},Bに使う竹{B_bn},Cに使う竹{C_bn}")
+    a_len=len(A_bn)
+    b_len=len(B_bn)
+    c_len=len(C_bn)
+    if a_len == 0 or b_len == 0 or c_len == 0:
+        xdebug("この竹からはいくら魔法を使っても作れません")
+        return MAXSIZE
+    mp=mp+10*(a_len-1)+10*(b_len-1)+10*(c_len-1)
+    a_sum=sum(A_bn)
+    b_sum=sum(B_bn)
+    c_sum=sum(C_bn)
+    xdebug(f"この方式だと Aの材料 {a_sum},Bの材料 {b_sum},Cの材料 {c_sum}になりますここから伸び縮み魔法を使います")
+    # TODO a_sum->A,b_sum->B,c_sum->Cにする伸び縮み魔法1を消費する
+    # TODO 2023-10-03 19:32:24
+    return mp
+
 for j0 in range(0,CH[0]):
     for j1 in range(0,CH[1]):
         for j2 in range(0,CH[2]):
@@ -55,16 +71,30 @@ for j0 in range(0,CH[0]):
                     for j5 in range(0,CH[5]):
                         for j6 in range(0,CH[6]):
                             for j7 in range(0,CH[7]):
-                                CLIST=[]
-                                CLIST.append(j0)
-                                CLIST.append(j1)
-                                CLIST.append(j2)
-                                CLIST.append(j3)
-                                CLIST.append(j4)
-                                CLIST.append(j5)
-                                CLIST.append(j6)
-                                CLIST.append(j7)
+                                CLIST=[0 for _ in range(0,8)]
+                                A_b = [] # Aに使う竹
+                                B_b = [] # Bに使う竹
+                                C_b = [] # Cに使う竹
+                                Z_b = [] # 使わない竹
+                                CLIST[0]=j0
+                                CLIST[1]=j1
+                                CLIST[2]=j2
+                                CLIST[3]=j3
+                                CLIST[4]=j4
+                                CLIST[5]=j5
+                                CLIST[6]=j6
+                                CLIST[7]=j7
                                 xdebug(CLIST)
                                 # https://docs.python.org/ja/3/tutorial/controlflow.html?highlight=match#tut-match
                                 # を使って各リストの中身をA_b,B_b,C_bに分ける
                                 # 分けた後その値を総値関数に投げて最小を受け取る
+                                for k in range(0,N):
+                                    if CLIST[k]==1:
+                                        A_b.append(L[k])
+                                    elif CLIST[k]==2:
+                                        B_b.append(L[k])
+                                    elif CLIST[k]==3:
+                                        C_b.append(L[k])
+                                    else:
+                                        Z_b.append(L[k])
+                                nowresult=magic(A_b,B_b,C_b)
