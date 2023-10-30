f8b18e100246a6e78fcfa6210d348020897d0110
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-10-30 19:16:34 +0900

    RMQ and RUQ

--- AOJ/segmenttree/DSL2_1_Range_Minumum_Query/no2_20231030/submit023d.py
+++ AOJ/segmenttree/DSL2_1_Range_Minumum_Query/no2_20231030/submit023d.py
@@ -30,13 +30,15 @@ ppp=pp.pprint
 # Const
 MAXSIZE = ( 1 << 59 ) -1
 MINSIZE = -( 1 << 59) + 1
+INF = (1<<31)-1
+
 class SegmentTree():
     n = 1
-    e = ((1<<31)-1)
+    e = INF
     sz = 0
     node = []
     def __init__(self,V):
-        self.sz = len(V)
+        self.sz=len(V)
         while self.n < self.sz:
             self.n=self.n*2
         self.node = [self.e]*(2*self.n-1)
@@ -49,49 +51,43 @@ class SegmentTree():
         self.node[x]=val
         while 0 < x:
             x = (x-1)//2
-            self.node[x]=min(self.node[2*x+1],self.node[2*x+2])
+            self.node[x]=min(self.node[x*2+1],self.node[x*2+2])
     def getmin(self,a,b,k=0,l=0,r=-1):
         if r < 0:
             r = self.n
         if r <= a or b <= l:
-            xdebug(f"結果 (r={r} <= a={a} or b={b} <= l={l})")
-            xdebug(f"もう a={a} <= l={l} and r={r} <= b={b}からかけ離れる")
-            if r <= a:
-                xdebug(f"検索の左側aが、調査の右側rに引っかかる")
-            if b <= l:
-                xdebug(f"検索の左側bが、調査の左側lに引っかかる")
-            xdebug(f"単位元を返す")
+        # pat1. l r<->a b
+        # pat2. a b<->l r
             return self.e
         if a <= l and r <= b:
-            xdebug(f"完全にlとrは被覆されている(a={a} <= l={l} and r={r} <= b={b})")
-            xdebug(f"ノード {k} の設定済み {self.node[k]}を返す")
+        # pat1. a l r b
             return self.node[k]
-        xdebug(f"??a={a} <= l={l} and r={r} <= b={b}??")
-        xdebug(f"Node {k} では まだ {a} と {b} との関係に不安が残る被覆かどうかまだ不明")
-        xdebug(f"そこでNode {2*k+1}に下って 左{l} と 右{(l+r)//2}を見ていく")
-        vl=self.getmin(a,b,2*k+1,l,(l+r)//2)
-        xdebug(f"Node {2*k+2}に下って 左{(l+r)//2} と右 {r} を見ていく")
-        vr=self.getmin(a,b,2*k+2,(l+r)//2,r)
+        vl = self.getmin(a,b,2*k+1,l,(l+r)//2)
+        vr = self.getmin(a,b,2*k+2,(l+r)//2,r)
         return min(vl,vr)
     def __str__(self):
-        ansl=[]
+        resultL=[]
         for j in range(0,2*self.n-1):
-            if self.node[j] != self.e:
-                ansl.append(self.node[j])
+            if self.node[j]==self.e:
+                resultL.append('E')
             else:
-                ansl.append('E')
-        return str(ansl)
+                resultL.append(self.node[j])
+        return str(resultL)
 
-N,Q = MI()
-A = [((1<<31)-1)]*N
-G = SegmentTree(A)
+N,Q=MI()
+A = [INF]*N
+G=SegmentTree(A)
+ANSL = []
 for j in range(0,Q):
-    query=tuple(MI())
-    if query[0] == 0:
-        dum,x,val=query
-        G.update(x,val)
-        xdebug(f"Gのノード:{G}")
+    query = tuple(MI())
+    if query[0]==0:
+        dum,pos,x=query
+        G.update(pos,x)
     else:
-        dum,s,t=query
-        ans=G.getmin(s,t+1)
-        print(ans)
+        dum,frm,to=query
+        # ANSL.append(f"{frm} から {to}までの最小値を求める")
+        x = G.getmin(frm,to+1)
+        ANSL.append(x)
+
+for line in ANSL:
+    print(line)
