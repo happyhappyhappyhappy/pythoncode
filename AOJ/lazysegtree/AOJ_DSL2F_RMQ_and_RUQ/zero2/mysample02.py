cadbafc0574a9dddf7bf3a78abc502aa32a700ee
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-11-04 18:01:54 +0900

    もう一回RUQとRMQをやってみた

--- AOJ/lazysegtree/AOJ_DSL2F_RMQ_and_RUQ/zero2/mysample02.py
+++ AOJ/lazysegtree/AOJ_DSL2F_RMQ_and_RUQ/zero2/mysample02.py
@@ -63,7 +63,7 @@ class LazySegment():
                 self.lazy[k*2+2]=self.lazy[k]
                 # 下に遅延フラグを立てる
                 self.lazyFlag[k*2+1]=True
-                self.lazyFlag[k*2+2]=False
+                self.lazyFlag[k*2+2]=True
             # このノードは遅延評価終了
             self.lazyFlag[k]=False
     def update(self,a,b,x,k=0,l=0,r=-1):
@@ -71,8 +71,40 @@ class LazySegment():
         # もし初期値であれば最右端のn(self.n)をセット
         if r < 0:
             r = self.n
-        # TODO: まず自分の遅延状況を評価する
-
+        self.eval(k,l,r)
+        if b <= l or r <= a:
+            # aとb,lとrが互いに絡まらないことが確定したら処理終了
+            return
+        if a <= l and r <= b:
+            # 包括しているときは、自身のノードを遅延箇所更新の上、評価
+            self.lazy[k]=x
+            self.lazyFlag[k]=True
+            self.eval(k,l,r)
+            return
+            # この今セットした遅延情報はeval内で更新して貰う
+        else:
+            # 混合している場合は、下位のノードに処理を願ってから値を貰う
+            self.update(a,b,x,k*2+1,l,(l+r)//2)
+            self.update(a,b,x,k*2+2,(l+r)//2,r)
+            self.node[k]=min(self.node[2*k+1],self.node[2*k+2])
+            return
+    def find(self,a,b,k=0,l=0,r=-1):
+        # 最小値を見つける
+        if r < 0:
+            # メインから呼び出したときはまず全体を取る
+            r = self.n
+        # まず自分を遅延評価
+        self.eval(k,l,r)
+        if b <= l or r <= a:
+            # 範囲外の場合は単位元を返す
+            return E
+        if a <= l and r <= b:
+            # 包括している場合は今のノードを返す
+            return self.node[k]
+        # 混合している場合は下の二つのノードに最小値発見の処理を行う
+        vl = self.find(a,b,2*k+1,l,(l+r)//2)
+        vr = self.find(a,b,2*k+2,(l+r)//2,r)
+        return min(vl,vr)
     def __str__(self):
         ansL=[]
         for j in range(0,2*self.n-1):
@@ -94,18 +126,25 @@ class LazySegment():
 N,Q = MI()
 V = [E]*N
 G = LazySegment(V)
-xdebug(f"G.node={G}")
-xdebug(f"G.lazy={G.strL()}")
+# xdebug(f"G.node={G}")
+# xdebug(f"G.lazy={G.strL()}")
 ANSL=[]
 for j in range(0,Q):
     query=tuple(MI())
     if query[0]==0:
         dmy,s,t,x=query
-        ANSL.append(f"{s}から{t+1}(閉区間)まで{x}を入れます")
-        # G.update(s,t+1,x)
+        msg=f"{s}から{t+1}(閉区間)まで{x}を入れます"
+        # xdebug(msg)
+        G.update(s,t+1,x)
+        # xdebug(f"G:{G}")
+        # xdebug(f"GL:{G.strL()}")
     else:
         dmy,s,t=query
-        ANSL.append(f"{s}から{t+1}(閉区間)までの最小値をもらいます")
-        # G.find(s,t+1)
+        msg=f"{s}から{t+1}(閉区間)までの最小値をもらいます"
+        # xdebug(f"今のG:{G}")
+        # xdebug(msg)
+        x=G.find(s,t+1)
+        ANSL.append(x)
+        # xdebug(x)
 for line in ANSL:
     print(line)
