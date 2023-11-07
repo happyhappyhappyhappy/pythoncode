1e4e7788cc9352d6ed4abe3e64dd3e7a3525234a
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-11-07 19:38:05 +0900

    一応こちらも終了

---
+++ AOJ/lazysegtree/AOJ_DSL2F_RMQ_and_RUQ/03/mysample03.py
@@ -0,0 +1,121 @@
+# ライブラリのインポート
+# これで終わりにするかもしれない
+import sys
+# import heapq,copy
+import pprint as pp
+# from collections import deque
+# pypy3用
+# import pypyjit
+# 再帰制御解放
+# pypyjit.set_param('max_unroll_recursion=-1')
+# sys.setrecursionlimit(10**6)
+from logging import getLogger, StreamHandler, DEBUG
+
+# 入力のマクロ
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+# デバッグ出力の作成
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+# クラス+メソッドを一関数
+xdebug=logger.debug
+ppp=pp.pprint
+# Const
+MAXSIZE = ( 1 << 59 ) -1
+MINSIZE = -( 1 << 59) + 1
+E = (1<<31)-1
+class LazySegment():
+    n=0
+    node=[]
+    lazy=[]
+    lazyFlag=[]
+    def __init__(self,V):
+        sv = len(V)
+        self.n = 1
+        while self.n < sv:
+            self.n=self.n*2
+        self.node=[E]*(self.n*2-1)
+        self.lazy=[E]*(self.n*2-1)
+        self.lazyFlag=[False]*(self.n*2-1)
+        for j in range(0,sv):
+            self.node[(self.n-1)+j]=V[j]
+        for j in range(self.n-2,-1,-1):
+            self.node[j]=min(self.node[2*j+1],self.node[2*j+2])
+    def eval(self,k,l,r):
+        if self.lazyFlag[k]==True:
+            self.node[k]=self.lazy[k]
+            if 1 < (r-l):
+                self.lazy[2*k+1]=self.lazy[k]
+                self.lazy[2*k+2]=self.lazy[k]
+                self.lazyFlag[2*k+1]=True
+                self.lazyFlag[2*k+2]=True
+            self.lazy[k]=E
+            self.lazyFlag[k]=False
+    def update(self,a,b,val,k=0,l=0,r=-1):
+        if r < 0:
+            r = self.n
+        self.eval(k,l,r)
+        if r <= a or b <= l:
+            return
+        if a <= l and r <= b:
+            self.lazy[k]=val
+            self.lazyFlag[k]=True
+            # 再評価!!
+            self.eval(k,l,r)
+        else:
+            mid = (l+r)>>1
+            self.update(a,b,val,2*k+1,l,mid)
+            self.update(a,b,val,2*k+2,mid,r)
+            self.node[k] = min(self.node[2*k+1],self.node[2*k+2])
+    def find(self,a,b,k=0,l=0,r=-1):
+        if r < 0:
+            r = self.n
+        if r <= a or b <= l:
+            return E
+        self.eval(k,l,r)
+        if a <= l and r <= b:
+            return self.node[k]
+        mid = (r+l)>>1
+        vl = self.find(a,b,2*k+1,l,mid)
+        vr = self.find(a,b,2*k+2,mid,r)
+        return min(vl,vr)
+    def __str__(self):
+        ans = ["nodeの中"]
+        for j in range(0,2*self.n-1):
+            if self.node[j] == E:
+                ans.append("E")
+            else:
+                ans.append(self.node[j])
+        return str(ans)
+    def strl(self):
+        ans = ["lazyの中"]
+        for j in range(0,self.n*2-1):
+            if self.lazy[j]==E:
+                ans.append("E")
+            else:
+                ans.append(self.lazy[j])
+        return str(ans)
+
+N,Q=MI()
+V=[E]*N
+G=LazySegment(V)
+ans=[]
+for j in range(0,Q):
+    query = tuple(MI())
+    if query[0]==0:
+        dmy,s,t,val=query
+        G.update(s,t+1,val)
+    else:
+        dmy,s,t=query
+        x = G.find(s,t+1)
+        ans.append(x)
+for line in ans:
+    print(line)
