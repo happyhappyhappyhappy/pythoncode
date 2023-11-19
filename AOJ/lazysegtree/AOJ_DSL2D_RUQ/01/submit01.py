37cd6aa4b44d5fe3aca4cd4305a3148698348532
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-11-19 13:49:36 +0900

    RUQ終了

--- AOJ/lazysegtree/AOJ_DSL2D_RUQ/01/submit01.py
+++ AOJ/lazysegtree/AOJ_DSL2D_RUQ/01/submit01.py
@@ -49,5 +49,66 @@ class LazySegment():
         for j in range(self.n-2,-1,-1):
             self.node[j]=min(self.node[j*2+1],self.node[j*2+2])
     def eval(self,k,l,r):
-# TODO: ここから AOJ_DSL2F\04\の解答をコピー
-# TODO: 2023-11-18 19:31:00
+        if self.lazyFlag[k]==False:
+            return
+        self.node[k]=self.lazy[k]
+        if 1 < (r-l):
+            self.lazy[2*k+1]=self.lazy[k]
+            self.lazy[2*k+2]=self.lazy[k]
+            self.lazyFlag[2*k+1]=True
+            self.lazyFlag[2*k+2]=True
+        self.lazy[k]=E
+        self.lazyFlag[k]=False
+    def update(self,a,b,val,k=0,l=0,r=-1):
+        if r < 0:
+            r = self.n
+        self.eval(k,l,r)
+        if r <= a or b <= l:
+            return
+        if a <= l and r <= b:
+            self.lazy[k]=val
+            self.lazyFlag[k]=True
+            self.eval(k,l,r)
+        else:
+            mid=(r+l)>>1
+            self.update(a,b,val,2*k+1,l,mid)
+            self.update(a,b,val,2*k+2,mid,r)
+            self.node[k]=min(self.node[2*k+1],self.node[2*k+1])
+    def getMin(self,a,b,k=0,l=0,r=-1):
+        if r < 0:
+            r = self.n
+        if r <= a or b <= l:
+            return E
+        self.eval(k,l,r)
+        if a <= l and r <= b:
+            return self.node[k]
+        mid = (l+r)>>1
+        vl = self.getMin(a,b,k*2+1,l,mid)
+        vr = self.getMin(a,b,k*2+2,mid,r)
+        return min(vl,vr)
+    def find(self,a):
+        return self.getMin(a,a+1)
+    def __str__(self):
+        ans = []
+        for j in range(0,self.n*2-1):
+            if self.node[j]==E:
+                ans.append("E")
+            else:
+                ans.append(self.node[j])
+        return str(ans)
+
+N,Q = MI()
+V = [E]*N
+G = LazySegment(V)
+ans = []
+for j in range(0,Q):
+    query=tuple(MI())
+    if query[0]==0:
+        _,s,t,val=query
+        G.update(s,t+1,val)
+    else:
+        _,s=query
+        val = G.find(s)
+        ans.append(val)
+for line in ans:
+    print(line)
