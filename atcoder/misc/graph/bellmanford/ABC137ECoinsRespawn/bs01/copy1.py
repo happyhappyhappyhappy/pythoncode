b7bec8fd4f50835685f332079a46f45c2683f993
happyhappyhappyhappy <ymnkkj@gmail.com>
2024-01-03 19:40:57 +0900

    とりあえずとっかかりは出来た

---
+++ atcoder/misc/graph/bellmanford/ABC137ECoinsRespawn/bs01/copy1.py
@@ -0,0 +1,48 @@
+# ライブラリのインポート
+import sys
+# import heapq,copy
+import pprint as pp
+# from collections import deque
+# pypy3用
+# import pypyjit
+# 再帰制御解放
+# pypyjit.set_param('max_unroll_recursion=-1')
+# sys.setrecursionlimit(10**6)
+from logging import getLogger, StreamHandler, DEBUG
+
+# 入力のマクロ
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+# デバッグ出力の作成
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+# クラス+メソッドを一関数
+xdebug=logger.debug
+ppp=pp.pprint
+# Const
+MAXSIZE = ( 1 << 59 ) -1
+MINSIZE = -( 1 << 59) + 1
+INF=1<<60
+
+N,M,P=MI()
+dist=[INF]*N
+edges=[]
+def BellmanFord(edges,dist,N,start):
+    return dist[N-1]
+
+for j in range(0,M):
+    a,b,c=MI()
+    a=a-1
+    b=b-1
+    c=(c-P)*(-1)
+    edges.append([a,b,c])
+ans=BellmanFord(edges,dist,N,0)
+print(ans)
