7b74d7ccae16d1623c9b4fb7ac26fb1ff0041d8b
happyhappyhappyhappy <ymnkkj@gmail.com>
2024-01-26 19:25:55 +0900

    ABC336B 初期終了

---
+++ atcoder/misc/bpro/ABC336B/01/submit01.py
@@ -0,0 +1,52 @@
+# ライブラリのインポート
+import sys
+# import heapq,copy
+import pprint as pp
+# from collections import deque
+# pypy3用
+# import pypyjit
+# 再帰制御解放
+# pypyjit.set_param('max_unroll_recursion=-1')
+# sys.setrecursionlimit(10**6)
+from logging import getLogger, StreamHandler, DEBUG
+
+# 入力のマクロ
+def II(): return int(sys.stdin.readline())
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+# デバッグ出力の作成
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+# クラス+メソッドを一関数
+xdebug=logger.debug
+ppp=pp.pprint
+# Const
+MAXSIZE = ( 1 << 59 ) -1
+MINSIZE = -( 1 << 59) + 1
+
+
+
+
+def solver(X):
+    result = 0
+    while True:
+        if X % 2 == 0:
+            result=result+1
+            X = X >> 1
+        else:
+            # xdebug(f"{X} が2で割れないのでやめる")
+            break
+    # algorithm
+    return result
+
+
+if __name__ == "__main__":
+    X = II()
+    print("{}".format(solver(X)))
