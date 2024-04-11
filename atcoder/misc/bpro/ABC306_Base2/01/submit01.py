c403f9c21d518c193a7aaddc1a552ca163b0b117
happyhappyhappyhappy <ymnkkj@gmail.com>
2024-04-11 19:35:46 +0900

    ABC306_Base2 のクリア

---
+++ atcoder/misc/bpro/ABC306_Base2/01/submit01.py
@@ -0,0 +1,54 @@
+# ライブラリのインポート
+# import heapq,copy
+import pprint as pp
+import sys
+
+# from collections import deque
+# pypy3用
+# import pypyjit
+# 再帰制御解放
+# pypyjit.set_param('max_unroll_recursion=-1')
+# sys.setrecursionlimit(10**6)
+from logging import DEBUG, StreamHandler, getLogger
+
+# 入力のマクロ
+def II(): return int(sys.stdin.readline())
+def SI(): return sys.stdin.readline().strip()
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number:int): return [LI() for _ in range(rows_number)]
+
+# デバッグ出力の作成
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+# クラス+メソッドを一関数
+xdebug=logger.debug
+ppp=pp.pprint
+# Const
+MAXSIZE = ( 1 << 59 ) -1
+MINSIZE = -( 1 << 59) + 1
+
+
+
+
+def solver():
+    result = 0
+    L=LI()
+    powtwo=[1]
+    for j in range(63):
+        x=powtwo[j]*2
+        powtwo.append(x)
+    for j in range(64):
+        if L[j]==1:
+            result=result+powtwo[j]
+    # algorithm
+    return result
+
+
+if __name__ == "__main__":
+    print(solver())
