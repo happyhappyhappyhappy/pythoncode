3052f544e1430e660a09c0fb676b56561a45734f
happyhappyhappyhappy <ymnkkj@gmail.com>
2024-04-12 19:41:08 +0900

    一通りもう一回

---
+++ atcoder/misc/bpro/ABC306B_Base2/02/unit/unit.py
@@ -0,0 +1,83 @@
+import os
+import sys
+import pprint as pp
+import unittest
+
+from io import StringIO
+
+# ライブラリのインポート
+# import heapq,copy
+
+# from collections import deque
+# pypy3用
+# import pypyjit
+# 再帰制御解放
+# pypyjit.set_param('max_unroll_recursion=-1')
+# sys.setrecursionlimit(10**6)
+from logging import DEBUG, StreamHandler, getLogger
+
+# 入力のマクロ
+def II(): return int(sys.stdin.readline())
+def SI(): return sys.stdin.readline().strip()
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number:int): return [LI() for _ in range(rows_number)]
+
+# デバッグ出力の作成
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+# クラス+メソッドを一関数
+xdebug=logger.debug
+ppp=pp.pprint
+# Const
+MAXSIZE = ( 1 << 59 ) -1
+MINSIZE = -( 1 << 59) + 1
+
+def solver():
+    twoBaseL=[1]
+    for j in range(63):
+        x=twoBaseL[j]*2
+        twoBaseL.append(x)
+    # strList="\n".join(map(str,twoBaseL))
+    # xdebug(strList)
+    # xdebug(pow(2,63))
+    L=LI()
+    ans=0
+    for j in range(64):
+        if L[j]==1:
+            ans=ans+twoBaseL[j]
+    return ans
+
+def resolve():
+    print(solver())
+
+class TestClass(unittest.TestCase):
+    def test_sample1(self):
+        input = """1 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"""
+        expected = """13"""
+        self.judge(input, expected)
+
+    def test_sample2(self):
+        input = """1 0 1 0 1 0 0 0 0 1 0 0 1 1 0 1 1 1 1 0 0 0 1 0 0 1 1 1 1 1 1 0 0 0 0 1 0 1 0 1 0 1 1 1 1 0 0 1 1 0 0 0 0 1 0 1 0 1 0 1 0 0 0 0"""
+        expected = """766067858140017173"""
+        self.judge(input, expected)
+
+    def judge(self, input, expected):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        actual = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(expected, actual)
+
+if __name__ == "__main__":
+    if "ATCODER" in os.environ:
+        resolve()
+    else:
+        unittest.main(verbosity=2)
