d9297ad2ccd3b64d7f6239a62a2d437834b1b970
happyhappyhappyhappy <ymnkkj@gmail.com>
2024-04-20 22:02:03 +0900

    限界

---
+++ atcoder/misc/live/ABC350/A/live/submitl.py
@@ -0,0 +1,52 @@
+# ライブラリのインポート
+# import heapq,copy
+import pprint as pp
+import sys
+
+# from collections import deque
+# pypy3用
+# import pypyjit
+# 再帰制御解放
+# pypyjit.set_param('max_unroll_recursion=-1')
+# sys.setrecursionlimit(10**6)
+from logging import DEBUG, StreamHandler, getLogger
+
+# 入力のマクロ
+def II(): return int(sys.stdin.readline())
+def SI(): return sys.stdin.readline().strip()
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number:int): return [LI() for _ in range(rows_number)]
+
+# デバッグ出力の作成
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+# クラス+メソッドを一関数
+xdebug=logger.debug
+ppp=pp.pprint
+# Const
+MAXSIZE = ( 1 << 59 ) -1
+MINSIZE = -( 1 << 59) + 1
+
+
+
+
+def solver():
+    ans="Yes"
+    S=SI()
+    S1=S[0:3]
+    S2=int(S[3:7])
+    if S1 != "ABC":
+        ans="No"
+    if 350 <= S2 or S2 == 316 or S2 == 0:
+        ans="No"
+    return ans
+
+
+if __name__ == "__main__":
+    print(solver())
