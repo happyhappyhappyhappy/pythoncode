c6e6bc92b2c497d47d4123b228ca219e57abaa99
happyhappyhappyhappy <ymnkkj@gmail.com>
2024-06-24 19:20:04 +0900

    ABC358 完了分

---
+++ atcoder/misc/live/ABC359/B/unit/unit01.py
@@ -0,0 +1,91 @@
+import os
+import sys
+import pprint as pp
+import unittest
+
+from io import StringIO
+
+# ライブラリのインポート
+# import heapq,copy
+
+# from collections import deque
+# pypy3用
+# import pypyjit
+# 再帰制御解放
+# pypyjit.set_param('max_unroll_recursion=-1')
+# sys.setrecursionlimit(10**6)
+from logging import DEBUG, StreamHandler, getLogger
+
+# 入力のマクロ
+def II(): return int(sys.stdin.readline())
+def SI(): return sys.stdin.readline().strip()
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number:int): return [LI() for _ in range(rows_number)]
+
+# デバッグ出力の作成
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+# クラス+メソッドを一関数
+xdebug=logger.debug
+ppp=pp.pprint
+# Const
+MAXSIZE = ( 1 << 59 ) -1
+MINSIZE = -( 1 << 59) + 1
+
+def solver():
+    result=0
+    N=II()
+    A=LI()
+    LT=[[] for _ in range(N+1)]
+    xdebug(f"LT={LT}")
+    for j in range(N*2):
+        x=A[j]
+        LT[x].append(j)
+    xdebug(f"LT={LT}")
+    for j in range(1,N+1):
+        if LT[j][1]-LT[j][0]==2:
+            result=result+1
+    return result
+
+def resolve():
+    print(solver())
+
+class TestClass(unittest.TestCase):
+    def test_sample1(self):
+        input = """3
+1 2 1 3 2 3"""
+        expected = """2"""
+        self.judge(input, expected)
+
+    def test_sample2(self):
+        input = """2
+1 1 2 2"""
+        expected = """0"""
+        self.judge(input, expected)
+
+    def test_sample3(self):
+        input = """4
+4 3 2 3 2 1 4 1"""
+        expected = """3"""
+        self.judge(input, expected)
+
+    def judge(self, input, expected):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        actual = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(expected, actual)
+
+if __name__ == "__main__":
+    if "ATCODER" in os.environ:
+        resolve()
+    else:
+        unittest.main(verbosity=2)
