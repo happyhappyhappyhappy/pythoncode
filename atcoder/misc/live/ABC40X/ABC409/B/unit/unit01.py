92096a290a90da82ec2ccdb74f205401a40b1b44
ymnkkj <ymnkkj@gmail.com>
2025-06-29 17:09:59 +0900

    ABC409B 完了

---
+++ atcoder/misc/live/ABC40X/ABC409/B/unit/unit01.py
@@ -0,0 +1,86 @@
+import os
+import sys
+import pprint as pp
+import unittest
+
+from io import StringIO
+
+# ライブラリのインポート
+# import heapq,copy
+
+# from collections import deque
+# pypy3用
+# import pypyjit
+# 再帰制御解放
+# pypyjit.set_param('max_unroll_recursion=-1')
+# sys.setrecursionlimit(10**6)
+from logging import DEBUG, StreamHandler, getLogger
+
+# 入力のマクロ
+def II(): return int(sys.stdin.readline())
+def SI(): return input().rstrip()
+def MI(): return map(int, sys.stdin.readline().split())
+def LI(): return list(map(int, sys.stdin.readline().split()))
+def LLI(rows_number:int): return [LI() for _ in range(rows_number)]
+
+# デバッグ出力の作成
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+# クラス+メソッドを一関数
+xdebug=logger.debug
+ppp=pp.pprint
+# Const
+MAXSIZE = ( 1 << 59 ) -1
+MINSIZE = -( 1 << 59) + 1
+
+def solver():
+    N=II()
+    A=LI()
+    res=MAXSIZE
+    for j in range(N,-1,-1):
+       cnt=0
+       for k in A:
+            if j <= k:
+               cnt+=1
+       if j <= cnt:
+           res=j
+           break
+#    xdebug(f"多分答えは {res}")
+    return res
+
+def resolve():
+    res=solver()
+    print(res)
+
+class TestClass(unittest.TestCase):
+    def test_sample1(self):
+        input = """3
+1 2 1"""
+        expected = """1"""
+        self.judge(input, expected)
+
+    def test_sample2(self):
+        input = """7
+1 6 2 10 2 3 2"""
+        expected = """3"""
+        self.judge(input, expected)
+
+    def judge(self, input, expected):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        actual = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(expected, actual)
+
+if __name__ == "__main__":
+    if "ATCODER" in os.environ:
+        resolve()
+    else:
+        unittest.main(verbosity=2)
